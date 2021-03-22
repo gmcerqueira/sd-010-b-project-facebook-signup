63b88f82267ab8cd66d14e316f2c50e3b57e7261
Antenor <antenorzapata@gmail.com>
2021-03-22 19:24:40 -0300

    requisito 18

--- script.js
+++ script.js
@@ -5,23 +5,25 @@ const inputEmail = document.querySelector('#user-email-phone');
 const loginForm = document.querySelector('.login-form');
 const inputs = loginForm.getElementsByTagName('input');
 const btnRegister = document.querySelector('#facebook-register');
-const verify = document.querySelector('.verify')
+// const verify = document.querySelector('.verify');
 
 btnLogin.addEventListener('click', () => {
   alert(inputEmail.value);
 });
 
-
-function validate () {
-    for (i = 0; i < inputs.length; i += 1 ){
-      if( inputs[i].value === '' || inputs.checked === false  ){
-        verify.innerHTML = 'Campos inválidos';        
-      }
+function validate() {
+  let count = 0;
+  for (let i = 0; i < inputs.length; i += 1) {
+    const ipt = inputs[i];
+    const value = ipt.type === 'radio' ? ipt.checked : ipt.value;
+    if (value) {
+      count += 1;
     }
+  }
+  return count >= 6;
 }
 
-
-btnRegister.addEventListener('click', function(e){
+btnRegister.addEventListener('click', (e) => {
   e.preventDefault();
-   validate();
-})
+  console.log(validate());
+});
