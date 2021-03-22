820de634cd57cfacc1aca5a7becc0db51a11f135
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-03-22 19:35:04 -0300

    Realizando correçao do requsito 20 do projeto

--- script.js
+++ script.js
@@ -66,12 +66,10 @@ cadastroBtn.addEventListener('click', (event) => {
 
   const rightContent = document.querySelector('.right-content');
   rightContent.innerHTML = `
-  <p>
     Olá, ${inputs[0]} ${inputs[1]} <br>
     Email ou telefone: ${inputs[2]} <br>
     Data de nascimento: ${inputs[4]} <br>
     Gênero: ${selecionarGender()}
-  </p>
   `;
 });
 
