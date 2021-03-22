0d81974a905ae12abde44c8daaf98f635722e459
walterlbf <walter.lbf.f@gmail.com>
2021-03-22 16:38:54 -0400

    eslint linhas 28 e 22

---
+++ script.js
@@ -0,0 +1,6 @@
+let inputEmail = document.getElementById("user-email-phone");
+let clickBotao = document.getElementById("button-login");
+
+clickBotao.addEventListener("click", function(){
+  alert(inputEmail.value);
+})
