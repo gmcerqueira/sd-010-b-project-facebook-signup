863d2e61efd8951a390160b46b7966e09c014bbb
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-03-22 17:33:34 -0300

    Adiciona o desafio 18 e concerta alguns erros de outros desafios

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function elertEmail() {
 }
 
 btnLogin.addEventListener('click', elertEmail);
+
+function checkInputValues () {
+  const inputs = document.getElementsByTagName('input');
+  let emptyInput = 0;
+  for(let i = 2; i < inputs.length; i += 1) {
+    if (inputs[i].value === '') {
+      emptyInput += 1;
+    }
+  }
+  return emptyInput;
+}
+
+const submitBtn = document.getElementById('facebook-register');
+submitBtn.addEventListener('click', () => {
+  if (checkInputValues() > 0) {
+    alert('Campos inválidos')
+  }
+});
