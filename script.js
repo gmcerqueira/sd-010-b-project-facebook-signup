ff9ac4b82b37dac322659555e6bc60f038fe16df
Fabio Marturano <fabiocmjor@gmail.com>
2021-03-22 17:56:38 -0300

    Validador Cadastre-se e Genero Personalizado

--- script.js
+++ script.js
@@ -8,11 +8,14 @@ const buttonCadastre = document.querySelector('#facebook-register');
 
 function checaForm() {
   const inputsToCheck = document.querySelectorAll('.AbraConta .toCheck');
+  const AbraContaForm = document.querySelector('.AbraConta');
 
   for (let input = 0; input < inputsToCheck.length; input += 1) {
     console.log(input);
     if (inputsToCheck[input].value === '') {
-      alert('Campos inválidos');
+      const aviso = document.createElement('p');
+      aviso.innerText = 'Campos inválidos';
+      AbraContaForm.append(aviso);
       break;
     }
   }
@@ -20,4 +23,15 @@ function checaForm() {
 
 buttonCadastre.addEventListener('click', () => {
   checaForm();
+});
+
+const genderSelect = document.querySelector('#genderSelect');
+const radioPerson = document.querySelector('#pers');
+
+radioPerson.addEventListener('click', () => {
+  const inputPerson = document.createElement('input');
+  inputPerson.id = 'Personalizado';
+  inputPerson.name = 'gender-custom';
+  inputPerson.placeholder = 'Gênero (opcional)';
+  genderSelect.appendChild(inputPerson);
 });
