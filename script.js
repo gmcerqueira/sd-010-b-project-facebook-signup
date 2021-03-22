ea10f17e268de1c621788ffad57345d558ea47ac
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-03-22 18:48:33 -0300

    FIX: 6 type of input

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const buttonLogin = document.querySelector('#button-login');
+const catchValueInput = document.querySelector("user-email-phone");
 buttonLogin.addEventListener('click', () => {
-  // console.log('teste');
-  alert('Email ou telefone');
+  console.log('teste');
+  alert(catchValueInput.value);
 });
