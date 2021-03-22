9908e51206ce6dc3689e3c67c06aae0414761379
Letycia Barreto <letyciasbarreto@gmail.com>
2021-03-22 18:00:56 -0300

    conserta lint

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.getElementById('button-login');
-function alertButton (){
+function alertButton() {
   const emailTel = document.getElementById('user-email-phone');
   alert(emailTel.value);
 }
