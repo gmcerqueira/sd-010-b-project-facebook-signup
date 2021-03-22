2f021d3f5538d108fd434ab0a1cb5610a197c7af
Abimael <abimaelr.albu@gmail.com>
2021-03-22 17:07:46 -0300

    Requisito 7 -- Abimael (lint)

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const btnEntrar = document.getElementById('button-login');
-const userLogin = document.getElementById('user-email-phone')
-btnEntrar.addEventListener('click', function(){
-    alert(userLogin.value);
+const btnEntrar = document.getElementById ('button-login');
+const userLogin = document.getElementById ('user-email-phone');
+
+btnEntrar.addEventListener ('click', function() {
+    alert (userLogin.value);
 })
