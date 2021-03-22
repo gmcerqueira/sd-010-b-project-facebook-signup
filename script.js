8074bc46cdd97b981e7b4fa0ad59b400c6d1995c
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-03-22 20:14:21 -0300

    Atualizar botao Salvar

--- script.js
+++ script.js
@@ -10,17 +10,14 @@ const buttonSalvar = document.querySelector('#facebook-register');
 buttonSalvar.addEventListener('click', () => {
   const input = document.querySelectorAll('input');
   for (let index = 2; index < (input.length - 3); index += 1) {
+    const genero = document.querySelector('input[name="gender"]:checked');
+    if (genero == null) {
+      alert('Campos inválidos');
+      break;
+    }
     if (input[index].value === '') {
       alert('Campos inválidos');
       break;
     }
   }
 });
-/*  const personalizado = document.querySelector('#facebook-register');
-
-personalizado.addEventListener('click', () =>{
-  const input = document.createElement('input')
-  input.type = 'text';
-  input.name = 'gender-custom';
-  input.placeholder = 'Gênero'
-})  */
