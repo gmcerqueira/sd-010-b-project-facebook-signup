4adeaaef2475517c92dd3c304394c934080c48dc
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-03-22 16:50:45 -0300

    Crie um subcontainer para colocar o conteúdo do lado esquerdo

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const buttonLogin = document.getElementById('button-login');
 
 buttonLogin.addEventListener('click', () => {
-  const entradaEmail = document.getElementById('user-email-phone');
-  if (entradaEmail.value == null) {
-    alert('Email ou telefone');
-  }
+  alert('Email ou telefone');
 });
