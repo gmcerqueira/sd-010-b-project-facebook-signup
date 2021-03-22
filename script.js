9908bba50b5bd45e2d8728dd19fb2f3e4a6ebbcc
Fabio Marturano <fabiocmjor@gmail.com>
2021-03-22 16:59:09 -0300

    requisito 18

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ const buttonLogin = document.querySelector('#button-login');
 buttonLogin.addEventListener('click', () => {
   alert(document.querySelector("#user-email-phone").value);
 });
+
+const buttonCadastre = document.querySelector('#facebook-register');
+
+buttonCadastre.addEventListener('click', () => {
+  const firstName = document.querySelector('#firstName');
+  const lastName = document.querySelector('#lastName');
+  const email = document.querySelector('#phoneEmail');
+  const password = document.querySelector('#password');
+  const birthdate = document.querySelector('#birthdate');
+
+  // alert(`${firstName.value}+${lastName.value}+${email.value}+${password.value}+${birthdate.value}`);
+
+  if((firstName.value == "")||(lastName.value == "")||(email.value == "")||(password.value == "")||(birthdate.value == "")){
+    alert('Campos inválidos');
+  }
+
+  // function genderCuston ()
+
+});
+
+// function genderCuston() {
+
+// }
