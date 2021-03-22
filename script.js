014a65457b251976a000437402b253a74c513f10
Fernando <fhomaia@gmail.com>
2021-03-22 17:58:58 -0300

    Requisitos 3,5,6 e 7

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function entrar() {
+    const btnentrar = document.getElementById('button-login');
+    const email = document.getElementById('user-email-phone-label');
+    btnentrar.addEventListener('click', function () {
+        alert email.innerText;
+    });
+}
+
+entrar();
