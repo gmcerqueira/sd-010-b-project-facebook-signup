52f4116afe3aa91b17bc5357d6c31464e7b22d41
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-03-22 19:14:45 -0300

    Validar se todos os campos foram preenchidos ao clicar no botão Cadastre-se

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const buttonLogin = document.querySelector('#button-login');
 buttonLogin.addEventListener('click', () => {
   alert('Email ou telefone');
 });
+
+const buttonSalvar = document.querySelector('#facebook-register');
+
+buttonSalvar.addEventListener('click', (event) => {
+  event.preventDefault();
+  const input = document.querySelectorAll('input');
+  for (let index = 2; index < (input.length - 3); index += 1) {
+    if (input[index].value === '') {
+      alert('Campos inválidos');
+      break;
+    }
+  }
+});
