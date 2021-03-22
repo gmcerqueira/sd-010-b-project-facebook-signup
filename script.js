17e2f6326ddeb9445fceef3783be96ca6faee793
Fabio Marturano <fabiocmjor@gmail.com>
2021-03-22 17:29:49 -0300

    Validador Campo Cadastre-se

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ buttonLogin.addEventListener('click', () => {
 
 const buttonCadastre = document.querySelector('#facebook-register');
 
+function checaForm() {
+  const inputsToCheck = document.querySelectorAll('.AbraConta .toCheck');
+
+  for (let input = 0; input < inputsToCheck.length; input += 1) {
+    console.log(input);
+    if (inputsToCheck[input].value === '') {
+      alert('Campos inválidos');
+      break;
+    }
+  }
+}
+
 buttonCadastre.addEventListener('click', () => {
-  // checaForm();
-
+  checaForm();
 });
-
-// function genderCuston() {
-
-// }
