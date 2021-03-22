5e89eb9fd215c6ac713bcccd64be16425bdd8898
Carlos Vieira <appencigr@gmail.com>
2021-03-22 18:22:36 -0300

    requisitos 7 feito

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //vamos utilizar js?
+// Sim Vamos inclusive no botâo Entrar!
+const btnEntrar = document.getElementById('button-login');
+let campEmail = document.getElementById('user-email-phone');
+
+btnEntrar.addEventListener('click', function () {
+  alert(campEmail.value);
+})
