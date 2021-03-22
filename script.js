9f2ac8dc71f35e66cc1317d6db42d00bece88ec5
Mariana Mohr <marianamohr@gmail.com>
2021-03-22 19:05:04 -0300

    ajustado erros de ESLint

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function alerta() {
-  alert("Email ou telefone");
+  alert('Email ou telefone');
 }
+function escutaBotao() {
+    document.getElementById('button-login').addEventListener('click', alerta);
+  }
 
-window.onload = function(){
-document.getElementById('button-login').addEventListener("click", alerta);
-}
+window.onload = escutaBotao();
