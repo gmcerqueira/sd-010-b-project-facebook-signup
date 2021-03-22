ad2238e4d3f824908212404db1b8941091bb0a8d
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-03-22 16:01:43 -0300

    Realizando requsito 8 do projeto

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const loginBtn = document.querySelector('#button-login');
 const user = document.querySelector('#user-email-phone');
 
 loginBtn.addEventListener('click', () => {
-  user.value ? alert(user.value) : alert('Usuário deve ser preenchido');
+  const res = user.value;
+  return alert(res);
 });
