c43221175c943ea379e73ffe6953106d6661f869
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-03-22 20:39:52 -0300

    Adicione um novo campo de texto no formulário se a pessoa usuária selecionar a opção Personalizado no campo Gênero

--- script.js
+++ script.js
@@ -7,17 +7,40 @@ buttonLogin.addEventListener('click', () => {
 
 const buttonSalvar = document.querySelector('#facebook-register');
 
-buttonSalvar.addEventListener('click', () => {
+buttonSalvar.addEventListener('click', (event) => {
+  event.preventDefault();
   const input = document.querySelectorAll('input');
+  const mensagem = document.querySelector('#mensagem');
   for (let index = 2; index < (input.length - 3); index += 1) {
     const genero = document.querySelector('input[name="gender"]:checked');
     if (genero == null) {
-      alert('Campos inválidos');
+      mensagem.innerHTML = 'Campos inválidos';
+      mensagem.style.color = 'red';
       break;
     }
     if (input[index].value === '') {
-      alert('Campos inválidos');
+      mensagem.innerHTML = 'Campos inválidos';
+      mensagem.style.color = 'red';
       break;
     }
   }
+});
+
+const personalizado = document.querySelector('#personalizado');
+const div = document.querySelector('#campo-personalizado');
+
+personalizado.addEventListener('click', () => {
+  const input = document.createElement('input');
+  const feminino = document.querySelector('#feminino');
+  const masculino = document.querySelector('#masculino');
+  input.type = 'text';
+  input.name = 'gender-custom';
+  input.placeholder = 'Gênero';
+  div.appendChild(input);
+  feminino.addEventListener('click', () => {
+    input.remove();
+  });
+  masculino.addEventListener('click', () => {
+    input.remove();
+  });
 });
