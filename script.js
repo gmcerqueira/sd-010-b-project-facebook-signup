4fcfb21beda783939699befdc79353ad3a399fbf
isTorres <ismaeltorres612@gmail.com>
2021-03-22 19:23:47 -0300

    commit 80%

--- script.js
+++ script.js
@@ -4,10 +4,3 @@ const userLogin = document.getElementById('user-email-phone');
 btnEntrar.addEventListener('click', () => {
   alert(userLogin.value);
 });
-
-// const register = document.getElementById('facebook-register');
-// // const inputForms = document.
-// register.addEventListener('click', (event) => {
-//   event.preventDefault();
-//   // if ()
-// });
