60a865a4cb425036a85d82dad862a265ce4d3496
Daniel <dsouzaborini@gmail.com>
2021-03-22 16:03:33 -0300

    requisito 7

---
+++ script.js
@@ -0,0 +1,6 @@
+const buttonLogin = document.getElementById("button-login");
+const email = document.getElementById("user-email-phone");
+
+const getEmailValue = () => alert(email.value);
+
+buttonLogin.addEventListener("click", getEmailValue);
