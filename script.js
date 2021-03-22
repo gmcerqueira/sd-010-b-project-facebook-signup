285d555c7d49f04c966ba152d197941a82edfe67
Abimael <abimaelr.albu@gmail.com>
2021-03-22 17:58:38 -0300

    corrigido o problema do lint

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const btnEntrar = document.getElementById('button-login');
-const   userLogin = document.getElementById('user-email-phone');
+const userLogin = document.getElementById('user-email-phone');
 
-btnEntrar.addEventListener ('click', function () {
-  alert (userLogin.value);
+btnEntrar.addEventListener('click', () => {
+  alert(userLogin.value);
 });
