8c8d0d0399867f95ac21cb4ddcb584fc38297c88
Mariana Savoldi <marianasavodi123@gmail.com>
2021-03-22 17:04:55 -0300

    Input senha type password e required no email e senha

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const loginButton = document.querySelector('#button-login');
 const inputEmail = document.querySelector('#user-email-phone-label')
 
 loginButton.addEventListener('click', function (){
-  alert(`${inputEmail.value}`)
+  alert(`${inputEmail.value}`);
 })
