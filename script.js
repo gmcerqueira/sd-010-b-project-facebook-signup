c0c245f0a19ee23dbfc91de080d320eba4907c24
hzozimo <hzozimo@gmail.com>
2021-03-22 17:37:42 -0300

    adequaçao de requisitos para o evaluator

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ function bttEntrar() {
 }
 
 bttEntrar();
+
+function mostrarElemento() {
+  const personalizado = document.getElementById('personalizado');
+  personalizado.styles.display = 'block';
+  
+}
+
+function adcEvntoGenPers() {
+  const personalizado = document.getElementById('personalizado');
+  personalizado.addEventListener('click', mostrarElemento);
+}
+
+adcEvntoGenPers();
