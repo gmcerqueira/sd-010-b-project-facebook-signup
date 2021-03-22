ca75730cef130c254c3826a1f97f4f1cba617825
Mariana Mohr <marianamohr@gmail.com>
2021-03-22 19:08:59 -0300

    ajustado erro arquivo JS

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 function alerta() {
   alert('Email ou telefone');
 }
-function escutaBotao() {
-    document.getElementById('button-login').addEventListener('click', alerta);
-  }
-
-window.onload = escutaBotao();
+document.getElementById('button-login').addEventListener('click', alerta);
