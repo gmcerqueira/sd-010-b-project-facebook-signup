7c1ec1095e97cd4aaf124f75b02278e3138fd7c9
lucasportella <lucasportellaagu@gmail.com>
2021-03-22 19:06:43 -0300

    requisito 7 com value

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const bot = document.getElementById('button-login');
 const inputEmailTel = document.getElementById('user-email-phone');
-bot.addEventListener('click', () => window.alert(inputEmailTel.innerText));
+bot.addEventListener('click', () => window.alert(inputEmailTel.value));
