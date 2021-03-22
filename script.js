3dbcd05d23cd12763870e346e80063c789a15c4e
Jessie <miss_sc_jessi@hotmail.com>
2021-03-22 18:11:39 -0300

    requisito 15 e 16arrumado

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ function clicaBotao() {
 }
 
 clicaBotao();
+
+//criar o aviso
+function criaAviso(aviso) {
+  
+  
+}
+
+const botaoCadastro = document.querySelector('#facebook-register');
+
+//verifica se os campos estão preenchidos
+function verificaCadastro() {
+  const div = document.querySelector('#verifica-cadastro');
+  const p = document.createElement('p');
+  const campoNome = document.querySelector('#nome').value;
+  const campoSobrenome = document.querySelector('#sobrenome').value;
+  const campoEmailPhone = document.querySelector('#email-phone').value;
+  const campoSenha = document.querySelector('#senha').value;
+  const campoDataNasc = document.querySelector('#birth-date').value;
+
+  if (campoNome || campoSobrenome || campoEmailPhone || campoSenha || campoDataNasc === null) {
+    div.appendChild(p).innerHTML = 'Campos Inválidos';
+  }
+}
+
+//clique do botão
+function clicaCadastro() {
+  botaoCadastro.addEventListener('click', verificaCadastro)
+}
+
+clicaCadastro();
