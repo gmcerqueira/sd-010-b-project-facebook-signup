318e96690455acf91f12cfb8db6158cc33f70e66
Jessie <miss_sc_jessi@hotmail.com>
2021-03-22 16:44:27 -0300

    requisito 7 com lint

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const botaoLogin = document.querySelector('#button-login');
 
 function clicaBotao() {
-  botaoLogin.addEventListener('click', function clica() {
-    const campoEmail = document.querySelector('#user-email-phone').value;
-    alert(campoEmail);
-  });
+  botaoLogin.addEventListener('click', clica);
+}
+
+function clica() {
+  const campoEmail = document.querySelector('#user-email-phone').value;
+  alert(campoEmail);
 }
 
 clicaBotao();
