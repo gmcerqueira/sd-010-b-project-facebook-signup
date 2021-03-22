6cbda9fbd031a46136760d4cf68e4ccf4ed69644
Letycia Barreto <letyciasbarreto@gmail.com>
2021-03-22 17:50:13 -0300

    adiciona script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const button = document.getElementById('button-login');
-function alertButton () {
-    const emailTel = document.getElementById('user-email-phone');
-    alert(emailTel.nodeValue);
+function alertButton (){
+  const emailTel = document.getElementById('user-email-phone');
+  alert(emailTel.value);
 }
 button.addEventListener('click', alertButton);
