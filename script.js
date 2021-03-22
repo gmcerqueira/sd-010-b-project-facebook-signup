3a5686165721fe9d009fb98c81ff1b2fbad7f70d
Thiago Marchini <thiago@msn.com>
2021-03-22 18:43:07 -0300

    correcoes quesito 10

--- script.js
+++ script.js
@@ -1,27 +1,49 @@
-function createNewParagraph() {
-  const paragraph = document.createElement('p');
-  paragraph.style.color = 'red';
-  paragraph.classList += 'invalid';
-  paragraph.innerText = 'Campos inválidos';
-  return paragraph;
-}
+// function createNewParagraph() {
+//   const paragraph = document.createElement('p');
+//   paragraph.style.color = 'red';
+//   paragraph.classList += 'invalid';
+//   paragraph.innerText = 'Campos inválidos';
+//   return paragraph;
+// }
 
-function validateForm() {
-  const inputs = document.querySelectorAll('input');
-  const paragraph = createNewParagraph();
-}
+// function validateForm() {
+//   const inputs = document.querySelectorAll('input');
+//   const paragraph = createNewParagraph();
+// }
 
-function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
+// function removeAllChildNodes(parent) {
+//   while (parent.firstChild) {
+//     parent.removeChild(parent.firstChild);
+//   }
+// }
+
+function createGenderInput() {
+  console.log('recebido');
+  const parent = document.getElementById('personalizedGender');
+  const input = document.createElement('input');
+  input.id = 'genderCustomText';
+  input.type = 'text';
+  input.name = 'gender-custom';
+  input.placeholder = 'Gênero (opcional)';
+  parent.appendChild(input);
 }
 
 document.getElementById('facebook-register').addEventListener('click',
   (event) => {
     event.preventDefault();
-    validateForm();
-    if (validateForm()) {
-      removeAllChildNodes(document.querySelector('.left-content'));
+    // validateForm();
+    // if (validateForm()) {
+    //   removeAllChildNodes(document.querySelector('.left-content'));
+    // }
+  });
+
+document.querySelectorAll('input[name="gender"]').forEach((elem) => {
+  console.log(elem);
+  elem.addEventListener('change', (event) => {
+    const inputValue = event.target.value;
+    const textBox = document.getElementById('genderCustomText');
+    if (inputValue === 'Personalizado' && !textBox) {
+      createGenderInput();
     }
   });
+});
