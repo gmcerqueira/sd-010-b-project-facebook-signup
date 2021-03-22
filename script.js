4f7a64290b7886b011e67e9853efad293765eed5
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-22 20:00:33 -0300

    muda o botão da barra nav

---
+++ script.js
@@ -0,0 +1,9 @@
+const generoPersonalizado = document.getElementById('other');
+generoPersonalizado.addEventListener('click', genPersonalizado);
+
+function genPersonalizado() {
+  const genPersonaliza = document.createElement('input');
+  genPersonaliza.type = 'text';
+  doc.appendChild
+  /* genPersonaliza.insertBefore(document.getElementById('facebook-register')); */
+}
