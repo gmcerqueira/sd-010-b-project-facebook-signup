fd3bca79950becb635492f7010b8953f67799a2d
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-03-22 18:09:07 -0300

    Realizando requsito 18 do projeto

--- script.js
+++ script.js
@@ -5,3 +5,34 @@ loginBtn.addEventListener('click', () => {
   const res = user.value;
   return alert(res);
 });
+
+const cadastroBtn = document.querySelector('#facebook-register');
+
+cadastroBtn.addEventListener('click', (event) => {
+  const erro = document.querySelector('#campos-invalidos');
+  erro.innerHTML = '';
+
+  const formCadastro = document.querySelector('#form-cadastro');
+
+  event.preventDefault();
+  const inputsCadastro = document.querySelectorAll('.input-cadastro');
+  for (let i = 0; i < inputsCadastro.length; i += 1) {
+    if (!inputsCadastro[i].value) {
+      erro.innerHTML = 'Campos inválidos';
+      formCadastro.appendChild(erro);
+      break;
+    }
+  }
+
+  const inputsGender = document.querySelectorAll('.input-gender');
+  let validaGender = false;
+  for (let i = 0; i < inputsGender.length; i += 1) {
+    if (inputsGender[i].checked) {
+      validaGender = true;
+    }
+  }
+  if (!validaGender) {
+    erro.innerHTML = 'Campos inválidos';
+    formCadastro.appendChild(erro);
+  }
+});
