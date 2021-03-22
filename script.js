36402e1dcd25ef027d882987abb2102965860506
Gabriel Essênio <gabriel.essenio@gmail.com>
2021-03-22 17:36:19 -0300

    Fazendo requisito 7 e funcao do botao entrar

---
+++ script.js
@@ -0,0 +1,8 @@
+function entrarFacebook(){
+let entrarBotao = document.getelementById(button-login);
+entrarBotao.addEventListener('click',function(event){
+    let emailCelular = document.getelementById(user-password);
+    alert("teste");
+})
+}
+entrarFacebook();
