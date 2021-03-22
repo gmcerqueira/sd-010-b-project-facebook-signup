c0d60885d5c389463b4d0f46e5e39442d0e1b0a8
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-03-22 20:58:21 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const buttonLogin = document.getElementById('button-login');
 
+buttonLogin.addEventListener('click', alerta);
+
 function alerta() {
   const userValue = document.getElementById('user-email-phone').value;
   alert(userValue);
 }
 
-buttonLogin.addEventListener('click', alerta);
+const registerButton = document.getElementById('facebook-register');
+
+registerButton.addEventListener('click', verify);
+
+let userInfoArray = document.querySelectorAll('.user-info');
+
+function verify () {
+  for (let index = 0; index < userInfoArray.length; index += 1) {
+    if (userInfoArray[index].value === "") {
+      alert("Campos inválidos");
+      break;
+    }
+  }
+}
