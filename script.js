f6cc94c4c94691ccab120395ce53b7cd48668b5b
Holistic <paulofreire.si@outlook.com>
2021-03-22 17:00:07 -0300

    req 2, again

--- script.js
+++ script.js
@@ -8,9 +8,15 @@ const header = document.createElement('header');
 header.className = 'top-bar';
 header.style.backgroundColor = 'rgb(66, 103, 178)';
 header.style.display = 'flex';
+header.style.justifyContent = 'space-evenly';
 spawn(header);
 
 const img = document.createElement('img');
 img.className = 'facebook-logo';
-img.src = './imgs/facebook-logo.png';
+img.src = 'imgs/facebook-logo.png';
 document.querySelector('.top-bar').appendChild(img);
+
+const forms = document.createElement('form');
+forms.className = 'facebook-login';
+forms.style.display = 'flex';
+document.querySelector('.top-bar').appendChild(forms);
