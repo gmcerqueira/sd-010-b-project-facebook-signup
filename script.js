44d78046269f730270b7e83486aaa1ccd5bf482c
Paulo Xavier <pxavierf@gmail.com>
2021-03-22 18:12:32 -0300

    requisito 18 - validando campos do form

--- script.js
+++ script.js
@@ -1,8 +1,86 @@
 function entrarFacebook(){
-let entrarBotao = document.getelementById("button-login");
-entrarBotao.addEventListener('click',function(){
-    alert("teste");
+let entrarBotao = document.getElementById("button-login");
+entrarBotao.addEventListener('click',function(){   
     console.log("teste");
 })
 }
 entrarFacebook();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Validando campos
+const inputText = document.querySelectorAll('.form input[type=text');
+const registerBtn = document.getElementById('facebook-register');
+
+registerBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const form = document.querySelector('.form');
+    const erro = document.createElement('p');
+    erro.innerText = 'Campos inválidos';
+    erro.style.color = 'red';
+    
+
+    for (let index = 0; index < inputText.length; index += 1) {
+        if (inputText[index].value === '') {
+            form.appendChild(erro);   
+        }
+    }        
+})  
+
+
+
+
+
+
+
