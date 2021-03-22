1da9dadbc8c0a0e14bf5493816a62ef14cdab525
Abimael <abimaelr.albu@gmail.com>
2021-03-22 18:31:41 -0300

    Alinhamentos do header - Abimael

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const userLogin = document.getElementById('user-email-phone');
 btnEntrar.addEventListener('click', () => {
   alert(userLogin.value);
 });
+
+const register = document.getElementById('facebook-register');
+// const inputForms = document.
+register.addEventListener('click', (event) => {
+  event.preventDefault();
+  
+});
