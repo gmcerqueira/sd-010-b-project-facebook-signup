ca9fc509c4d3c3bd9d8f2e7ceb4a64edd752ca82
Johnny-Batata <johnny.arguello007@gmail.com>
2021-03-22 19:10:06 -0300

    requisito 18.1

--- script.js
+++ script.js
@@ -2,7 +2,26 @@
 
 const btnLogin = document.querySelector('#button-login');
 const inputEmail = document.querySelector('#user-email-phone');
+const loginForm = document.querySelector('.login-form');
+const inputs = loginForm.getElementsByTagName('input');
+const btnRegister = document.querySelector('#facebook-register');
+const verify = document.querySelector('.verify')
 
 btnLogin.addEventListener('click', () => {
   alert(inputEmail.value);
 });
+
+
+function validate () {
+    for (i = 0; i < inputs.length; i += 1 ){
+      if( inputs[i].value === '' || inputs.checked === false  ){
+        verify.innerHTML = 'Campos inválidos';        
+      }
+    }
+}
+
+
+btnRegister.addEventListener('click', function(e){
+  e.preventDefault();
+   validate();
+})
