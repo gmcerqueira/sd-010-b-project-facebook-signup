9e0f8665bc3b2ee497683a89c30434e87661a0b8
Matheus H. Gois <matheus.hengo@gmail.com>
2021-03-22 17:31:33 -0300

    Es:lint arrumado

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const button = document.getElementById('button-login');
-function alertButton () {
-    const emailTel = document.getElementById('user-email-phone');
-    alert(emailTel.nodeValue);
+
+function alertButton() {
+  const emailTel = document.getElementById('user-email-phone');
+  alert(emailTel.nodeValue);
 }
 button.addEventListener('click', alertButton);
