df3368c909f7dc3c7876dd0a30c80403717583e3
Arlen Freitas <arlenfreitas1@gmail.com>
2021-03-22 18:12:50 -0300

    estiliza botoes e forms

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const btnEntrar = document.getElementById('button-login');
 
-btnEntrar.addEventListener('click', function(){
+btnEntrar.addEventListener('click', function() {
   alert('Email ou Telefone');
 });
