6f64a1b194a4f8f06a378282d1aa0510bd55e5a9
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-03-22 17:52:09 -0300

    Adiciona js para alert do requisito 7

---
+++ script.js
@@ -0,0 +1,8 @@
+const longinButton = document.getElementById('button-login');
+
+longinButton.addEventListener('click', () => {
+  const inputName = document.getElementById('user-email-phone').value;
+  if (inputName) {
+    alert(inputName);
+  }
+});
