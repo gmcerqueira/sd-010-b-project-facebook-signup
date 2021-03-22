d5cdcd637261d44670aae75f94dbaf471bf65612
Fernando <fhomaia@gmail.com>
2021-03-22 20:09:11 -0300

    Erros lint

--- script.js
+++ script.js
@@ -30,11 +30,12 @@
 
 
 function entrar() {
-    const btnentrar = document.getElementById('button-login');
-    const email = document.getElementById('user-email-phone-label');
-    btnentrar.addEventListener('click', function () {
-        alert(email.value);
-    });
+  const btnentrar = document.getElementById('button-login');
+  const email = document.getElementById('user-email-phone-label');
+  btnentrar.addEventListener('click',alertemail);
+  function alertemail() {
+    alert(email.value); 
+  }   
 }
 
 entrar();
