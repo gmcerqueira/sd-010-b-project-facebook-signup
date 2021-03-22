cc575add6b1c0872ac84f5b464f21ff118a2f023
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-03-22 19:11:44 -0300

    FIX: 8. flex-direction: row

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const buttonLogin = document.querySelector('#button-login');
-const catchValueInput = document.querySelector("user-email-phone");
+const catchValueInput = document.querySelector('#user-email-phone');
+console.log(catchValueInput);
 buttonLogin.addEventListener('click', () => {
+	if (catchValueInput.value == null) {
+    alert('Campo Vazio');
+  }
   console.log('teste');
+	console.log(catchValueInput.value);
   alert(catchValueInput.value);
 });
