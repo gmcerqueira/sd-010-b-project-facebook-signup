61e9306b4d654f2a3d7da002cfcd499a5be8171f
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-03-22 16:42:46 -0300

    7. Adicione um subcontainer com o botão Entrar

---
+++ script.js
@@ -0,0 +1,7 @@
+inputEmailPhone = document.getElementById('user-email-phone');
+
+buttonGetIn = document.getElementById('button-login');
+
+buttonGetIn.addEventListener('click', function() {
+    alert(inputEmailPhone.value);
+})
