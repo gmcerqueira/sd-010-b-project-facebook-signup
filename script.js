81c6496a886d258bf7c5a26b7b5f0f329efae589
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-03-22 20:35:49 -0300

    Adiciona função para alerta

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ function genPersonalizado() {
   genPersonaliza.type = 'text';
   doc.appendChild;
   /* genPersonaliza.insertBefore(document.getElementById('facebook-register')); */
+
+const btnLogin = document.getElementById('button-login');
+const entradaEmail = document.getElementById('user-email-phone');
+
+btnLogin.addEventListener('click', () => {
+  alert(entradaEmail.value);
+});
