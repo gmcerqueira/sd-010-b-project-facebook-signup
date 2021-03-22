06209613af0b23e1103d28f942b3c5daead6c7b9
Fernando <fhomaia@gmail.com>
2021-03-22 20:18:35 -0300

    Erros Lint Script

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-function alertemail() {
-  alert(email.value);
-}
-
 function entrar() {
   const btnentrar = document.getElementById('button-login');
   const email = document.getElementById('user-email-phone-label');
+  function alertemail() {
+    alert(email.value);
+  }
   btnentrar.addEventListener('click', alertemail);
 }
 
