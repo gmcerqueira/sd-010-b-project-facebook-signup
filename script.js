c81dbd5f4d47282b3cc54d7964753d812572d477
renatogsouza <renatomacedo1995@gmail.com>
2021-03-22 17:06:53 -0300

    correçao lint javascript

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const btnEntrar = document.getElementById('button-login');
 
-btnEntrar.addEventListener('click', function(){
+btnEntrar.addEventListener('click', alerta);
+
+function alerta() {
   alert('Email ou Telefone');
-});
+}
