6bd6a1de0f64e6263c512dbdbdc491787f5acf84
Renan Antunes <renan.antunes.rocha@gmail.com>
2021-03-22 16:18:02 -0300

    Concerta erros de linter

---
+++ script.js
@@ -0,0 +1,6 @@
+let btnLogin = document.getElementById("button-login");
+let txtEmailtel = document.getElementById("email-tel");
+
+btnLogin.addEventListener("click", function () {
+    alert(txtEmailtel.value);
+});
