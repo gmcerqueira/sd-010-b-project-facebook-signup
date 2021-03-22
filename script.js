e117a83a193b6807a633b716094f117931a41e33
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-03-22 18:50:19 -0300

    exercicio 16

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const textAlert = document.getElementById('user-email-phone');
 button.addEventListener('click', () => {
   alert(textAlert.value);
 });
+
+const opcaoPersonalizada = document.getElementById('personalizado');
+opcaoPersonalizada.addEventListener('click', () => {
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.name = 'gender-custom';
+  input.placeholder = 'Gênero (opcional)';
+});
