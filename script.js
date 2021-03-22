91cd5c1891b20b544c26a32b4c713ab2687fac23
Dieghonm <dieghonm@gmail.com>
2021-03-22 18:49:29 -0300

    teste de compartilhamento 390

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const inputEmailPhone = document.getElementById('user-email-phone');
 
 const buttonGetIn = document.getElementById('button-login');
 
-buttonGetIn.addEventListener('click', function() {
+buttonGetIn.addEventListener('click', botão)
+
+function botão() {
     alert(inputEmailPhone.value);
 })
