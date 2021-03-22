c2c0b0c1dbc1ba395012fc31050397641b25e481
Daniel <dsouzaborini@gmail.com>
2021-03-22 18:07:02 -0300

    add script

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttonLogin = document.getElementById('button-login');
+const email = document.getElementById('user-email-phone');
+
+const getEmailValue = () => alert(email.value);
+
+buttonLogin.addEventListener('click', getEmailValue);
+//tira delete
