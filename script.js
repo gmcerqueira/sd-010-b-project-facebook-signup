e969dc7b080c2dd018e31d0a7492eb957bfb6786
Fernando <fhomaia@gmail.com>
2021-03-22 19:24:06 -0300

    Parcial dos requisitos 10 ao 16

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function entrar() {
     const btnentrar = document.getElementById('button-login');
     const email = document.getElementById('user-email-phone-label');
     btnentrar.addEventListener('click', function () {
-        alert email.innerText;
+        alert(email.value);
     });
 }
 
