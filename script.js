fa23171751a5ed56cef84de45a906b6cccf89b70
Matheus H. Gois <matheus.hengo@gmail.com>
2021-03-22 23:25:14 -0300

    Requisito 19

--- script.js
+++ script.js
@@ -4,4 +4,28 @@ function alertButton() {
   const emailTel = document.getElementById('user-email-phone');
   alert(emailTel.value);
 }
-button.addEventListener('click', alertButton);
+
+const customGenderRadio = document.getElementById('custom');
+const customGenderInput = document.getElementById('gender-custom');
+const genderRadio = document.querySelectorAll('input[name=gender]');
+
+function radioEventClick() {
+  for (let element = 0; element < genderRadio.length; element += 1) {
+    genderRadio[element].addEventListener('click', (e) => {
+      elementToggleVisibility(e);
+    })
+  }
+}
+
+function elementToggleVisibility(event) {
+  if (event.toElement === customGenderRadio) {
+    customGenderInput.classList.remove('hidden');
+  } else {
+    customGenderInput.classList.add('hidden');
+  }
+}
+
+window.onload = function () {
+  button.addEventListener('click', alertButton);
+  radioEventClick();
+}
