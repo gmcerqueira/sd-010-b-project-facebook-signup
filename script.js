b3a5040ebc2d28b49f8b6c4912ea43aec1051e5d
Emerson Saturnino <emersonsaturnino@pop-os.localdomain>
2021-03-23 01:03:40 -0300

    Finalizando o projeto

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-const nome = document.getElementById('nomeLogin').value;
-const sobrenome = document.getElementById('sobrenomeLogin').value;
-const telefone = document.getElementById('telefoneLogin').value;
-const nascimento = document.getElementById('nascimentoLogin').value;
-const senha = document.getElementById('senhaLogin').value;
-const elementos = [nome, sobrenome, telefone, nascimento, senha];
 const botao = document.getElementById('facebook-register');
 const botaoLogin = document.getElementById('button-login');
 const personalizadoRadio = document.getElementById('personalizado');
 const personalizado = document.getElementById('genero-personalizado');
+const campo = document.getElementById('campos-invalidos');
+const rightForm = document.querySelector('.right-content');
+
+window.onload = function () {
+  botaoLogin.addEventListener('click', alertEmail);
+  botao.addEventListener('click', checarCampo);
+  personalizadoRadio.addEventListener('click', mostrarElemento);
+  botao.addEventListener('click', mostrarElemento);
+};
 
 function mostrarElemento() {
   personalizado.style.display = 'flex';
 }
 
 function checarCampo() {
-  for (let index = 0; index < elementos.length; index += 1) {
-    if (elementos[index] === '') {
-      const campo = document.getElementById('campos-invalidos');
+  const formInputs = document.querySelectorAll('.input-cadastro');
+  for (let index = 0; index < formInputs.length; index += 1) {
+    if (formInputs[index] === '') {
       campo.style.display = 'flex';
       campo.innerText = 'Campos inválidos';
       break;
     }
+  }
+  if (campo.innerText != 'Campos inválidos') {
+    userLoged();
   }
 }
 
@@ -28,8 +34,19 @@ function alertEmail() {
   alert(`${document.getElementById('user-email-phone').value}`);
 }
 
-window.onload = function () {
-  botaoLogin.addEventListener('click', alertEmail);
-  botao.addEventListener('click', checarCampo);
-  personalizadoRadio.addEventListener('click', mostrarElemento);
-};
+function userLoged() {
+  const formInputs = document.querySelectorAll('.input-cadastro');
+  const fullName = document.createElement('h1');
+  fullName.innerText = `Olá, ${formInputs[0].value} ${formInputs[1].value}`;
+  const emailPhone = document.createElement('p');
+  emailPhone.innerText = formInputs[2].value;
+  const birthday = document.createElement('p');
+  birthday.innerText = formInputs[4].value;
+  const genderUser = document.createElement('p');
+  genderUser.innerText = document.querySelector('input[type="radio"]:checked').value;
+  rightForm.innerHTML = '';
+  rightForm.appendChild(fullName);
+  rightForm.appendChild(emailPhone);
+  rightForm.appendChild(birthday);
+  rightForm.appendChild(genderUser);
+}
