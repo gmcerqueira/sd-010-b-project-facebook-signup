aa78d5f2cb816e0dfa26246581417dd7a917aead
Caio <thehaseol@gmail.com>
2021-03-22 23:43:20 -0300

    requisito 7, primeira tentativa

---
+++ script.js
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-unused-vars
+function enviar() {
+  const valor = document.getElementById('user-email-phone').value;
+  alert(`Olá, ${valor}, seja bem vindo de volta.`);
+}
+/* uma função que recebe o valor colocado dentro do input do login e mostra na tela por meio de um alert */
