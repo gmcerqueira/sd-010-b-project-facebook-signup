4f46041a6d0a262ebe9e018e4cda56d9829317d7
Carlos <cemargato@gmail.com>
2021-03-23 14:34:22 -0300

    acessando o botao entrar

---
+++ script.js
@@ -0,0 +1,8 @@
+let entrarButton = document.getElementById('button-login');
+
+function alertMessage(evento) {
+  evento.preventDefault();
+  alert(document.getElementById('user-email-phone').value);
+}
+
+entrarButton.addEventListener('click', alertMessage);
