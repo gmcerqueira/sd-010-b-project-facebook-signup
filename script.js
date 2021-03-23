44844ee2cffd4c0a76a66349cd09393c9114b750
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-03-22 21:02:44 -0300

    lint

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const buttonLogin = document.getElementById('button-login');
 
-buttonLogin.addEventListener('click', alerta);
-
 function alerta() {
   const userValue = document.getElementById('user-email-phone').value;
   alert(userValue);
 }
 
+buttonLogin.addEventListener('click', alerta);
+
 const registerButton = document.getElementById('facebook-register');
 
-registerButton.addEventListener('click', verify);
+const userInfoArray = document.querySelectorAll('.user-info');
 
-let userInfoArray = document.querySelectorAll('.user-info');
-
-function verify () {
+function verify() {
   for (let index = 0; index < userInfoArray.length; index += 1) {
-    if (userInfoArray[index].value === "") {
-      alert("Campos inválidos");
+    if (userInfoArray[index].value === '') {
+      alert('Campos inválidos');
       break;
     }
   }
 }
+
+registerButton.addEventListener('click', verify);
