f92bd92f00ba702c5a0f0976e6a2c2bc6d8ff500
Lotar Lucas <amaral.lucas.lotar@gmail.com>
2021-03-23 20:39:21 -0300

    Alteraçoes feitas para a nova versao

--- script.js
+++ script.js
@@ -3,10 +3,6 @@
 // O novo campo dever ser uma campo de texto com o atributo name "gender-custom" e um placeholder "Gênero (opcional)"
 // O novo campo deve estar posicionado entre as opções de gênero e o botão "Cadastrar-se"
 
-document.getElementById('Per').addEventListener('click', mostrar);
-document.getElementById('Mas').addEventListener('click', esconder);
-document.getElementById('Fem').addEventListener('click', esconder);
-
 function esconder() {
   document.getElementById('genderPersonalizado').style.display = 'none';
 }
@@ -14,3 +10,7 @@ function esconder() {
 function mostrar() {
   document.getElementById('genderPersonalizado').style.display = 'block';
 }
+
+document.getElementById('Per').addEventListener('click', mostrar);
+document.getElementById('Mas').addEventListener('click', esconder);
+document.getElementById('Fem').addEventListener('click', esconder);
