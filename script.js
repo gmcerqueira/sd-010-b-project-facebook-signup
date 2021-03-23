0d335f06302f916ef96a2ee0ed8aaf5539719fab
Lara Capila <laracapilak@gmail.com>
2021-03-23 10:38:37 -0300

    corrige erros lint

--- script.js
+++ script.js
@@ -14,42 +14,40 @@ buttonEntrar.addEventListener('click', botaoEntrar);
 
 function validar(evento) {
   evento.preventDefault();
-
-  const name = document.getElementById("name");
-  const lastname = document.getElementById("lastname");
-  const phone_email = document.getElementById("phone_email");
-  const password = document.getElementById("password");
-  const birthdate = document.getElementById("birthdate");
+  const name = document.getElementById('name');
+  const lastname = document.getElementById('lastname');
+  const phone_email = document.getElementById('phone_email');
+  const password = document.getElementById('password');
+  const birthdate = document.getElementById('birthdate');
   const radio = document.querySelector('input[name="gender"]:checked');
-  
-    if (name.value === "") {
-      alert("Nome não informado");
-      name.focus();
-      return;
-    }
-    if (lastname.value === "") {
-      alert("Sobrenome não informado");
-      lastname.focus();
-      return;
-    }
-    if (phone_email.value === "") {
-      alert("E-mail não informado");
-      phone_email.focus();
-      return;
-    }
-    if (password.value === "") {
-      alert("Senha não informada");
-      password.focus();
-      return;
-    }
-    if (birthdate.value === "") {
-      alert("Aniversário não informado");
-      birthdate.focus();
-      return;
-    }
-    if (radio === null) {
-      alert("Marque um gênero");
-      return;
-    }
+  if (name.value === '') {
+    alert('Nome não informado');
+    name.focus();
+    return;
   }
+  if (lastname.value === '') {
+    alert('Sobrenome não informado');
+    lastname.focus();
+    return;
+  }
+  if (phone_email.value === '') {
+    alert('E-mail não informado');
+    phone_email.focus();
+    return;
+  }
+  if (password.value === '') {
+    alert('Senha não informada');
+    password.focus();
+    return;
+  }
+  if (birthdate.value === '') {
+    alert('Aniversário não informado');
+    birthdate.focus();
+    return;
+  }
+  if (radio === null) {
+    alert('Marque um gênero');
+    return;
+  }
+}
   form.addEventListener('submit', validar);
