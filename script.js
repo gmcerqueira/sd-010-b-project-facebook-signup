018943190af6b2e1f94dcd91c2b9165354b42e7f
Mariana Mohr <marianamohr@gmail.com>
2021-03-23 11:44:26 -0300

    Alterado de let para const variavel email nafunçao alerta

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function alerta() {
-  let email = document.getElementById('user-email-phone').value;
+  const email = document.getElementById('user-email-phone').value;
   alert(email);
 }
+
 document.getElementById('button-login').addEventListener('click', alerta);
