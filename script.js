536ee729e140073439744ad6708368c618c82539
Emerson Saturnino <emersonsaturnino@pop-os.localdomain>
2021-03-23 01:20:05 -0300

    Finalizando o projeto

--- script.js
+++ script.js
@@ -5,20 +5,32 @@ const personalizado = document.getElementById('genero-personalizado');
 const campo = document.getElementById('campos-invalidos');
 const rightForm = document.querySelector('.right-content');
 
+window.onload = function () {
+  botaoLogin.addEventListener('click', alertEmail);
+  botao.addEventListener('click', checarCampo);
+  personalizadoRadio.addEventListener('click', mostrarElemento);
+  botao.addEventListener('click', mostrarElemento);
+};
+
 function mostrarElemento() {
   personalizado.style.display = 'flex';
 }
 
 function checarCampo() {
-  const formInputs = document.querySelectorAll('.input-cadastro');
-  for (let index = 0; index < formInputs.length; index += 1) {
-    if (formInputs[index] === '') {
+  const nome = document.getElementById('nomeLogin').value;
+  const sobrenome = document.getElementById('sobrenomeLogin').value;
+  const telefone = document.getElementById('telefoneLogin').value;
+  const nascimento = document.getElementById('nascimentoLogin').value;
+  const senha = document.getElementById('senhaLogin').value;
+  const elementos = [nome, sobrenome, telefone, nascimento, senha];
+  for (let index = 0; index < elementos.length; index += 1) {
+    if (elementos[index] === '') {
       campo.style.display = 'flex';
       campo.innerText = 'Campos inválidos';
       break;
     }
   }
-  if (campo.innerText !== 'Campos inválidos') {
+  if (campo.innerText != 'Campos inválidos') {
     userLoged();
   }
 }
@@ -36,18 +48,11 @@ function userLoged() {
   const birthday = document.createElement('p');
   birthday.innerText = formInputs[4].value;
   const genderUser = document.createElement('p');
-  let vI = document.querySelector('input[type="radio"]:checked').value;
-  genderUser.innerText = vI;
+  let valorR = document.querySelector('input[type="radio"]:checked').value;
+  genderUser.innerText = valorR;
   rightForm.innerHTML = '';
   rightForm.appendChild(fullName);
   rightForm.appendChild(emailPhone);
   rightForm.appendChild(birthday);
   rightForm.appendChild(genderUser);
 }
-
-window.onload = function () {
-  botaoLogin.addEventListener('click', alertEmail);
-  botao.addEventListener('click', checarCampo);
-  personalizadoRadio.addEventListener('click', mostrarElemento);
-  botao.addEventListener('click', mostrarElemento);
-};
