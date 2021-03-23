882b5d4efe151901262cc96cc62ba28ef836b289
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-03-23 19:51:20 -0300

    refatoraçao requisito 20

--- script.js
+++ script.js
@@ -42,22 +42,17 @@ const deleteDiv = document.getElementById('delete');
 const createElement = document.createElement('div');
 
 function showData() {
-  for (let index = 0; index < userInfoArray.length; index += 1) {
-    if (userInfoArray[index] !== '') {
-      dataKey.firstN = document.getElementById('firstname').value;
-      dataKey.lastN = document.getElementById('lastname').value;
-      dataKey.phoneEmail = document.getElementById('phone-email').value;
-      dataKey.birthDate = document.getElementById('birthdate').value;
-      dataKey.gen = document.querySelector('input[type="radio"]:checked').value;
-      createElement.innerHTML = `<p>Olá, ${dataKey.firstN} ${dataKey.lastN}</p>
+  dataKey.firstN = document.getElementById('firstname').value;
+  dataKey.lastN = document.getElementById('lastname').value;
+  dataKey.phoneEmail = document.getElementById('phone-email').value;
+  dataKey.birthDate = document.getElementById('birthdate').value;
+  dataKey.gen = document.querySelector('input[type="radio"]:checked').value;
+  createElement.innerHTML = `<p>Olá, ${dataKey.firstN} ${dataKey.lastN}</p>
       <p>${dataKey.phoneEmail}</p>
       <p>${dataKey.birthDate}</p>
       <p>${dataKey.gen}</p>`;
-      rightContent.removeChild(deleteDiv);
-      rightContent.appendChild(createElement);
-      break;
-    }
-  }
+  rightContent.removeChild(deleteDiv);
+  rightContent.appendChild(createElement);
 }
 
 registerButton.addEventListener('click', showData);
