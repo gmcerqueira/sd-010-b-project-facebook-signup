1c7fc4688bc441968b351076c561e6dc018ee38f
edersonrodara <edersonrodara@gmail.com>
2021-03-23 08:39:42 -0300

    dando continuidade

---
+++ script.js
@@ -0,0 +1,7 @@
+function test() {
+  if (document.getElementById('user-email-phone').value === '') {
+    alert('digite o login');
+  }
+}
+
+document.getElementById('button-login').addEventListener('click', test);
