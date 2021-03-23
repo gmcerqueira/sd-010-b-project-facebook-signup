c6cd6f2874b06603cd73294b4ea2fce94b52ff26
Michelle <mls.srbek@gmail.com>
2021-03-23 17:18:17 -0300

    created function to create the custom gender, but had a lot of issues

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 function emailTelephoneAlert() {
   const emailTel = document.getElementById('user-email-phone');
-  const item = String(emailTel.value);
-  alert(item);
+  alert(emailTel.value);
+}
+
+function customGender() {
+  const custom = document.createElement('input');
+  custom.name = 'gender-custom';
+  custom.placeholder = 'Gênero (opcional)';
+  const parent = document.getElementsByClassName('genders')[0];
+  parent.appendChild(custom);
 }
 
 window.onload = function () {
   const buttonLogin = document.getElementById('button-login');
   buttonLogin.addEventListener('click', emailTelephoneAlert);
+
+  const genderCustom = document.getElementById('custom-gender');
+  genderCustom.addEventListener('click', customGender);
 };
