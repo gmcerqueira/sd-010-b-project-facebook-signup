4d0d942e46697836ac29eb36ec12770c3c62368a
Caio <thehaseol@gmail.com>
2021-03-23 00:05:16 -0300

    requisito 7, tentativa de uma funçao diferente

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-// eslint-disable-next-line no-unused-vars
+/*
 function enviar() {
   const valor = document.getElementById('user-email-phone').value;
-  alert(`Olá, ${valor}, seja bem vindo de volta.`);
+  alert(`${valor}`);
 }
-/* uma função que recebe o valor colocado dentro do input do login e mostra na tela por meio de um alert */
+ uma função que recebe o valor colocado dentro do input do login e mostra na tela por meio de um alert
 
 enviar();
+*/
+const input = document.querySelector('#user-email-phone');
+
+function messageAlert() {
+  alert(input.value);
+}
+
+const btn = document.querySelector('#button-login');
+btn.addEventListener('click', messageAlert);
