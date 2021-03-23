50378a50b622900dcd6e45b25b1d1daff4d4913e
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-03-22 22:09:32 -0300

    FIX: Lint, Lint Styles and edits the header style

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const buttonLogin = document.querySelector('#button-login');
 const catchValueInput = document.querySelector('#user-email-phone');
-console.log(catchValueInput);
+
 buttonLogin.addEventListener('click', () => {
-	if (catchValueInput.value == null) {
+  if (catchValueInput.value !== '') {
+    alert(catchValueInput.value);
+  } else {
     alert('Campo Vazio');
   }
-  console.log('teste');
-	console.log(catchValueInput.value);
-  alert(catchValueInput.value);
 });
