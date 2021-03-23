f959a3c69a132040104e0192766ca46ac5192d78
edersonrodara <edersonrodara@gmail.com>
2021-03-23 11:02:34 -0300

    requisito 7 - botao com alert

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-function teste() {
-  if (document.getElementById('user-email-phone').value === '') {
-    alert('Email ou telefone');
-  }
+function buttonLogin() {
+  alert('Email ou telefone');
 }
-document.getElementById('button-login').addEventListener('click', teste);
+document.getElementById('button-login').addEventListener('click', buttonLogin);
