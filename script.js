897f32a8d953e85a43d0836dfd92af4377778039
Paulo Xavier <pxavierf@gmail.com>
2021-03-23 12:33:41 -0300

    versao anterior

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-const form = document.querySelector('.form');
-const erro = document.createElement('p');
-
-const firstName = document.getElementById('first-name-form');
-const lastName = document.getElementById('last-name-form');
-const phoneEmail = document.getElementById('phone-email-form');
-const password = document.getElementById('password-form');
-const birthDay = document.getElementById('date-birthdate-form');
-const otherInput = document.getElementById('other-input')
-
-const genderOtherDiv = document.querySelector('.gender-other');
-const radios = document.querySelectorAll('input[type=radio]');
-
-const botaoCadastro = document.getElementById('facebook-register');
-const rightContent = document.querySelector('.right-content');
-
 function entrarFacebook() {
   const entrarBotao = document.getElementById('button-login');
   entrarBotao.addEventListener('click', () => {
@@ -23,61 +7,51 @@ function entrarFacebook() {
 }
 entrarFacebook();
 
-function erroText() {
-  erro.innerText = 'Campos Inválidos';
-  erro.style.color = 'red';
-}
+const getPrimeiroNome = document.getElementById('first-name-form').value;
+const getUltimoNome = document.getElementById('last-name-form').value;
+const getEmailCelular = document.getElementById('phone-email-form').value;
+const dateBirthdate = document.getElementById('date-birthdate-form');
+const getDataNascimento = dateBirthdate.value;
+const botaoCadastro = document.getElementById('facebook-register');
 
-// botão
+function substituiConteudo() {
+  botaoCadastro.addEventListener('click', () => {
+    const nomeCompleto = document.createElement('h2');
+    nomeCompleto.innerText = `Olá, ${getPrimeiroNome} ${getUltimoNome}`;
+    const emailCelular = document.createElement('p');
+    emailCelular.innerText = getEmailCelular;
+    const dataNascimento = document.createElement('p');
+    dataNascimento.innerText = getDataNascimento;
+    const opcaoGenero = document.createElement('p');
+    const inputRadio = document.querySelector('input[type="radio"]:checked');
+    opcaoGenero.innerText = inputRadio.value;
+    const formDireita = document.querySelector('.right-content');
+    formDireita.innerHTML = '';
+    formDireita.appendChild(nomeCompleto);
+    formDireita.appendChild(emailCelular);
+    formDireita.appendChild(dataNascimento);
+    formDireita.appendChild(opcaoGenero);
+  });
+}
+substituiConteudo();
+// Validando campos
+const inputText = document.querySelectorAll('.form input[type=text');
+
 botaoCadastro.addEventListener('click', (e) => {
   e.preventDefault();
-  erro.innerText = '';
-  validacao();
-  addValueContent ();
+  const form = document.querySelector('.form');
+  const erro = document.createElement('p');
+  erro.innerText = 'Campos inválidos';
+  erro.style.color = 'red';
+  for (let index = 0; index < inputText.length; index += 1) {
+    if (inputText[index].value === '') {
+      form.appendChild(erro);
+    }
+  }
 });
-
-function addValueContent () {
-  const inputs = document.querySelectorAll('.form input');
-  if (erro.innerText === '') {
-    rightContent.innerHTML = '';
-    for (let index = 0; index < inputs.length; index += 1) {
-      if (inputs[index].type === 'radio') {
-        if (!inputs[index].checked) {
-          continue;
-        }
-        if (inputs[index].id === 'other') {
-          continue;
-        }
-      }
-      createRightText(inputs[index]);
-    }
-  }
-}
-
-//criando texto do right-content
-function createRightText(input) {
-  const createPara = document.createElement('p');
-  createPara.innerText = input.value;
-  rightContent.appendChild(createPara);
-}
-
-// validação
-function validacao(){
-  const validarInput = [firstName, lastName, phoneEmail, password, birthDay];
-  for (let index = 0; index < validarInput.length; index += 1) {
-    if (validarInput[index].value === '') {
-      erroText();
-    }
-  }
-  form.appendChild(erro);
-  const radioOther = document.querySelector('input[type=radio]:checked');
-  if (radioOther.id === 'other' && otherInput.value === '') {
-    erroText();
-  }
-  form.appendChild(erro);
-}
-
 //  campo personalizar genero
+const genderOtherDiv = document.querySelector('.gender-other');
+const radios = document.querySelectorAll('input[type=radio]');
 radios.forEach((radio) => {
   radio.addEventListener('click', () => {
     if (radio.id === 'other') {
