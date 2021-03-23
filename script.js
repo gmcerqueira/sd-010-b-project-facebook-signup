21115f833287540d33e708b0eec65d719222bed9
Junior030 <juniorhenrique030@gmail.com>
2021-03-23 20:30:23 -0300

    Versao 2.0

---
+++ script.js
@@ -0,0 +1,16 @@
+// Ao clicar no botão com o id #button-login deve exibir um alert com o valor do campo "Email ou telefone"
+
+// O novo campo dever ser uma campo de texto com o atributo name "gender-custom" e um placeholder "Gênero (opcional)"
+// O novo campo deve estar posicionado entre as opções de gênero e o botão "Cadastrar-se"
+
+document.getElementById('Per').addEventListener('click', mostrar);
+document.getElementById('Mas').addEventListener('click', esconder);
+document.getElementById('Fem').addEventListener('click', esconder);
+
+function esconder() {
+  document.getElementById('genderPersonalizado').style.display = 'none';
+}
+
+function mostrar() {
+  document.getElementById('genderPersonalizado').style.display = 'block';
+}
