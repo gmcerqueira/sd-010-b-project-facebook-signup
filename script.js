c05105127fdc795dfb9fe4cb6ed983f5b7e80e11
Antenor <antenorzapata@gmail.com>
2021-03-22 21:11:25 -0300

    teste final

--- script.js
+++ script.js
@@ -7,11 +7,41 @@ const inputs = loginForm.getElementsByTagName('input');
 const btnRegister = document.querySelector('#facebook-register');
 const btnPersonalizado = document.querySelector('.Personalizado');
 const register = document.querySelector('.register');
-// const verify = document.querySelector('.verify');
+const verify = document.querySelector('.verify');
+const divData = document.createElement('div');
+const divRightContent = document.querySelector('.right-content');
 
 btnLogin.addEventListener('click', () => {
   alert(inputEmail.value);
 });
+
+const showMessage = (values) => {
+  const message = `Olá, ${values[0]} ${values[1]}`;
+  divData.append(message);
+
+  const len = values.length > 5 ? 5 : values.length;
+
+  for (let i = 2; i < len; i += 1) {
+    const p = document.createElement('p');
+    p.innerText = values[i];
+    divData.appendChild(p);
+  }
+  divRightContent.appendChild(divData);
+  divRightContent.removeChild(loginForm);
+};
+
+const showData = () => {
+  const values = [];
+
+  for (let i = 0; i < inputs.length; i += 1) {
+    if (inputs[i].type === 'text') {
+      values.push(inputs[i].value);
+    } else if (inputs[i].checked === true) {
+      values.push(inputs[i].className);
+    }
+  }
+  showMessage(values);
+};
 
 function validate() {
   let count = 0;
@@ -22,12 +52,18 @@ function validate() {
       count += 1;
     }
   }
+  console.log(count);
   return count >= 6;
 }
 
 btnRegister.addEventListener('click', (e) => {
   e.preventDefault();
-  console.log(validate());
+
+  if (validate()) {
+    showData();
+  } else {
+    verify.innerText = 'Campos inválidos';
+  }
 });
 
 const generateCustom = () => {
