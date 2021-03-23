a62107dc50f5ff948981bba9785b4ba93dfaa20b
André De Bem <andrebemferraz@gmail.com>
2021-03-23 11:48:29 -0300

    Request8

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ buttonId.addEventListener('click', () => {
   const valueFirst = document.getElementById('user-email-phone').value;
   alert(valueFirst);
 });
+
+// Estava com dificuldade para passar no Lint, pois sempre ficava apontando uma função inesperada. Depois de olhar alguns códigos de alguns colegos utilizei o => ao invés da função e o Lint aceitou.
