5e26ceb6a408b17afaf5bfec1f9e6f2270e7534f
Lara Capila <laracapilak@gmail.com>
2021-03-23 20:30:38 -0300

    teste verificações2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const buttonEntrar = document.getElementById('button-login');
 
-const form = document.getElementById('forms');
+// const form = document.getElementById('forms');
 
 function botaoEntrar() {
   const emailTel = document.getElementById('user-email-phone').value;
@@ -12,16 +12,16 @@ buttonEntrar.addEventListener('click', botaoEntrar);
 const buttonCadastrar = document.getElementById('facebook-register');
 // const userName = document.getElementById('name');
 // const lastName = document.getElementById('lastname');
-// const phoneEmail = document.getElementById('phone_email'); 
-// const password = document.getElementById('password'); 
-// const birthdate = document.getElementById('birthdate'); 
+// const phoneEmail = document.getElementById('phone_email');
+// const password = document.getElementById('password');
+// const birthdate = document.getElementById('birthdate');
 const radioSelecionado = document.querySelector('input[name="gender"]:checked');
-console.log(radioSelecionado)
+console.log(radioSelecionado);
 
 function validarCadastro() {
-  if (radioSelecionado.value === null) { 
+  if (radioSelecionado.value === null) {
     alert('Campo inválido');
   }
 }
 
-buttonCadastrar.addEventListener('click', validarCadastro); 
+buttonCadastrar.addEventListener('click', validarCadastro);
