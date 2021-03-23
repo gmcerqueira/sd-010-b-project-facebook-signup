aea5f593f2047da42faf982fbeaf6e9ea68c0ce9
Guilherme Lima <guilhermelimasky@gmail.com>
2021-03-23 00:23:52 -0300

    Adicionando evento ao button

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-// Beleza aderson
-// Olá Guilherme
+const email = document.getElementById('user-email-phone')
+const buttonHeader = document.getElementById('button-login')
+
+buttonHeader.addEventListener('click', buttonH)
+
+function buttonH (){
+    alert(email.value)
+}
