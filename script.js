e4589524eb8e496df64d0534a68d2a00e4f847e2
Emerson Saturnino <emersonsaturnino@pop-os.localdomain>
2021-03-23 00:19:29 -0300

    Criando o Alert

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
+window.onload = function () {
 const nome = document.getElementById('nomeLogin').value;
 const sobrenome = document.getElementById('sobrenomeLogin').value;
 const telefone = document.getElementById('telefoneLogin').value;
 const nascimento = document.getElementById('nascimentoLogin').value;
 const senha = document.getElementById('senhaLogin').value;
+const elementos = [nome, sobrenome, telefone, nascimento, senha];
+const botao = document.getElementById('facebook-register');
+const botaoLogin = document.getElementById('button-login');
+const personalizadoRadio = document.getElementById('personalizado');
+const personalizado = document.getElementById('genero-personalizado');
 
-function mostrarElemento() {
-  const personalizado = document.getElementById('genero-personalizado');
+function mostrarElemento() {  
   personalizado.style.display = 'flex';
 }
 
-function adcEvntoGenPers() {
-  const personalizado = document.getElementById('personalizado');
-  personalizado.addEventListener('click', mostrarElemento);
-}
-
-adcEvntoGenPers();
-
-function checarCampo() {
-  const elementos = [nome, sobrenome, telefone, nascimento, senha];
-
+function checarCampo() {  
   for (let index = 0; index < elementos.length; index += 1) {
     if (elementos[index] === '') {
       const campo = document.getElementById('campos-invalidos');
@@ -29,9 +25,12 @@ function checarCampo() {
   }
 }
 
-function validarCadastro() {
-  const botao = document.getElementById('facebook-register');
-  botao.addEventListener('click', checarCampo);
+function alertEmail() {
+  alert(`${document.getElementById('user-email-phone').value}`);
 }
 
-validarCadastro();
+botaoLogin.addEventListener('click', alertEmail);
+botao.addEventListener('click', checarCampo);
+personalizadoRadio.addEventListener('click', mostrarElemento);
+
+}
