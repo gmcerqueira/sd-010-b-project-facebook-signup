10a546499041336b3a5ac2ab65f1e7f3837a74ec
root <diegoserafim1@gmail.com>
2021-03-23 12:02:33 -0300

    criado estilo para agrupar botoes de entrar no topo lado direito

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btnEntrar = document.getElementById('button-login');
 const emailTelefone = document.getElementById('user-email-phone');
 
 // verifica se a pessoa digitou o email e clicou em entrar
+// font de pesquisa > https://blog.betrybe.com/javascript/javascript-alert/
 function entrar() {
   const emailPessoa = emailTelefone.value;
   if (emailPessoa === '') {
