49b0e46915d7afa6884106efbb173a8f3c69dac7
Matheus H. Gois <matheus.hengo@gmail.com>
2021-03-23 20:36:38 -0300

    Requisito 19, Bonus e arrumado alguns requisitos

--- script.js
+++ script.js
@@ -9,14 +9,7 @@ const customGenderRadio = document.getElementById('custom');
 const customGenderInput = document.getElementById('gender-custom');
 const genderRadio = document.querySelectorAll('input[name=gender]');
 
-function radioEventClick() {
-  for (let element = 0; element < genderRadio.length; element += 1) {
-    genderRadio[element].addEventListener('click', (e) => {
-      elementToggleVisibility(e);
-    })
-  }
-}
-
+/* Verifica se o elemento "linkado" é igual à um determinado elemento, caso sim remove uma classe, se não, a remove. */
 function elementToggleVisibility(event) {
   if (event.toElement === customGenderRadio) {
     customGenderInput.classList.remove('hidden');
@@ -26,7 +19,16 @@ function elementToggleVisibility(event) {
   }
 }
 
-window.onload = function () {
+// Adiciona eventos de clique para os radio-buttons
+function radioEventClick() {
+  for (let element = 0; element < genderRadio.length; element += 1) {
+    genderRadio[element].addEventListener('click', (e) => {
+      elementToggleVisibility(e);
+    });
+  }
+}
+
+window.onload = () => {
   button.addEventListener('click', alertButton);
   radioEventClick();
-}
+};
