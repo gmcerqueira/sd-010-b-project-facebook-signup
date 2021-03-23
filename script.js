604fe527c612614e25d3e1882734308c109ca3ea
Michelle <mls.srbek@gmail.com>
2021-03-23 16:37:16 -0300

    created a function to display an alert with email or telephone - lint corrections

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ function emailTelephoneAlert() {
   const emailTel = document.getElementById('user-email-phone-label');
   const item = String(emailTel.value);
   alert(item);
-};
+}
 
 window.onload = function () {
-  document.getElementById('button-login').addEventListener('click', emailTelephoneAlert);
+    const buttonLogin = document.getElementById('button-login');
+  buttonLogin.addEventListener('click', emailTelephoneAlert);
 };
