d39e77993721cf78cc5548f3c4868c0ee423762f
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-03-22 21:58:05 -0300

    requisito 18

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.getElementById('button-login').addEventListener('click', () => {
   alert(document.getElementById('user-email-phone').value);
 });
+
+document.getElementById('facebook-register').addEventListener('click', () => {
+  const forms = document.getElementById('main-form');
+  const Invalido = document.createElement('p');
+  const inputs = document.getElementsByTagName('input');
+  for (let x = 2; x < inputs.length; x += 1) {
+    if (inputs[x].value === '') {
+      forms.appendChild(Invalido);
+      Invalido.innerText = 'Campos inválidos';
+    }
+  }
+});
