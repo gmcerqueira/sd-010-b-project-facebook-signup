bd4f5cdacb25ea5b9ac884396a8a9e013c41be23
limaluizpaulo <limaluizpaulo@live.com>
2021-03-23 00:59:04 -0300

    Validar se todos os campos foram preenchidos ok

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function entrar() {
 }
 
 btnEntrar.addEventListener('click', entrar);
+
+const registerBtn = document.querySelector('#facebook-register');
+const campos = document.querySelectorAll('.input');
+
+registerBtn.addEventListener('click', function () {
+  for (let index = 0; index < campos.length; index += 1) {
+    if (campos[index].value === '') {
+      campos[index].value = 'Campos inválidos';
+    }
+  }
+});
