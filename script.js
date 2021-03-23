cc4fe4814e6ad967b98f315090939938f62a84eb
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-03-23 14:00:52 -0300

    correção lint

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const login = document.getElementById('button-login');
 
 const message = document.getElementById('user-email-phone');
 
-login.addEventListener('click',clickLogin);
+login.addEventListener('click', clickLogin);
 
 function clickLogin() {
   alert(message.value);
