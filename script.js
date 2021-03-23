8f98f7e27c9e6e29dd199d4fab9dadf3cc6552cf
isTorres <ismaeltorres612@gmail.com>
2021-03-23 00:18:03 -0300

    requisito 17 e inicio do desenvolvimento dos requisitos 18 e 19

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ const userLogin = document.getElementById('user-email-phone');
 btnEntrar.addEventListener('click', () => {
   alert(userLogin.value);
 });
+
+const btnRegister = document.getElementById('facebook-register');
+const inputArray = document.getElementById('name-password');
+const inputs = inputArray.getElementsByTagName('input');
+const form = document.getElementById('register');
+const span = document.createElement('span');
+
+btnRegister.addEventListener('click', (event) => {
+  for (let i = 0; i < inputs.length; i++) {
+    if(inputs[i].value === ''){
+      event.preventDefault();
+      inputs.value = '';
+      span.innerHTML = 'Campos inválidos';
+      form.append(span);
+      // em desenvolvimento
+    } else {
+        continue;
+    }
+  }
+});
+
+const otherGender = document.getElementsByName('gender')[2];
+if (otherGender.checked) {
+  const creatInput = document.createElement('input');
+  creatInput.type = 'text';
+  creatInput.name = 'gender'
+  // em desenvolvimento
+}
