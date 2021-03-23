6bf85893adea4127a8a146bd637a381ab7c49204
esionascimento <esio.392@protonmail.com>
2021-03-23 19:14:22 -0300

    block label genero

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const consButton = document.querySelector('#button-login');
+const consButtonRegis = document.querySelector('#facebook-register');
+const body = document.querySelector('.right-content');
 
 consButton.addEventListener('click', () => {
   const aux = document.querySelector('#user-email-phone').value;
   alert(aux);
+});
+
+consButtonRegis.addEventListener('click', () => {
+  const auxP = document.createElement('p');
+  const constValidar = document.querySelector('.input-text').value;
+  console.log(constValidar);
+  auxP.innerText = constValidar;
+  body.appendChild(auxP);
 });
