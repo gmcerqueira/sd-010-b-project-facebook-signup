a363b2029bc85cbc7b2b503fc91db2045b6000c6
Igor Machado <rf59imft@gmail.com>
2021-03-23 08:36:51 -0300

    desafio 15

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ function login() {
 }
 
 login();
+
+// function createDate() {
+//   let getSelect = document.querySelector('.select-date');
+//   let createOptionDate = document.createElement('option');
+//   createOptionDate.className = 'option-days';
+
+//   getSelect.appendChild(createOptionDate);
+// }
+
+// createDate();
+
+// function addDays() {
+//   let optionDay = document.querySelector('.option-days');
+//   let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//   for (let item = 0; item < array.length; item += 1) {
+//     optionDay.innerText += array[item];
+//   }
+// }
+
+// addDays();
