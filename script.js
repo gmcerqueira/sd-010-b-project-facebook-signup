f8ed3dc9e9955854150c80de5f1b0ee0207df3c1
Dieghonm <dieghonm@gmail.com>
2021-03-23 13:47:29 -0300

    arruma quesito 18, tentativa com onsubmit no form 3

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ buttonGetIn.addEventListener('click', buttonIn);
 // https://www.w3schools.com/js/js_validation.asp
 // function validateForm() {
 //   var x = document.forms["Form"]["firstname"].value;
-//   if (x == "") {
+//   if (x === "") {
 //     alert("Name must be filled out");
 //     return false;
 //   }
