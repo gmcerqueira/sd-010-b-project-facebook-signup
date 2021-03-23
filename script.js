b0cbbdc9b5f0b2b9c387228a5b4269b331caa133
Emerson Saturnino <emersonsaturnino@pop-os.localdomain>
2021-03-23 00:20:19 -0300

    Criando o Alert

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
 window.onload = function () {
-const nome = document.getElementById('nomeLogin').value;
-const sobrenome = document.getElementById('sobrenomeLogin').value;
-const telefone = document.getElementById('telefoneLogin').value;
-const nascimento = document.getElementById('nascimentoLogin').value;
-const senha = document.getElementById('senhaLogin').value;
-const elementos = [nome, sobrenome, telefone, nascimento, senha];
-const botao = document.getElementById('facebook-register');
-const botaoLogin = document.getElementById('button-login');
-const personalizadoRadio = document.getElementById('personalizado');
-const personalizado = document.getElementById('genero-personalizado');
+  const nome = document.getElementById('nomeLogin').value;
+  const sobrenome = document.getElementById('sobrenomeLogin').value;
+  const telefone = document.getElementById('telefoneLogin').value;
+  const nascimento = document.getElementById('nascimentoLogin').value;
+  const senha = document.getElementById('senhaLogin').value;
+  const elementos = [nome, sobrenome, telefone, nascimento, senha];
+  const botao = document.getElementById('facebook-register');
+  const botaoLogin = document.getElementById('button-login');
+  const personalizadoRadio = document.getElementById('personalizado');
+  const personalizado = document.getElementById('genero-personalizado');
 
-function mostrarElemento() {  
-  personalizado.style.display = 'flex';
-}
+  function mostrarElemento() {
+    personalizado.style.display = 'flex';
+  }
 
-function checarCampo() {  
-  for (let index = 0; index < elementos.length; index += 1) {
-    if (elementos[index] === '') {
-      const campo = document.getElementById('campos-invalidos');
-      campo.style.display = 'flex';
-      campo.innerText = 'Campos inválidos';
-      break;
+  function checarCampo() {
+    for (let index = 0; index < elementos.length; index += 1) {
+      if (elementos[index] === '') {
+        const campo = document.getElementById('campos-invalidos');
+        campo.style.display = 'flex';
+        campo.innerText = 'Campos inválidos';
+        break;
+      }
     }
   }
-}
 
-function alertEmail() {
-  alert(`${document.getElementById('user-email-phone').value}`);
-}
+  function alertEmail() {
+    alert(`${document.getElementById('user-email-phone').value}`);
+  }
 
-botaoLogin.addEventListener('click', alertEmail);
-botao.addEventListener('click', checarCampo);
-personalizadoRadio.addEventListener('click', mostrarElemento);
-
-}
+  botaoLogin.addEventListener('click', alertEmail);
+  botao.addEventListener('click', checarCampo);
+  personalizadoRadio.addEventListener('click', mostrarElemento);
+};
