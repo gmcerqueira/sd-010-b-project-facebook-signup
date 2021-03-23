aa277a0bcb499d18eaa01a11c68ab6ea81085534
Thiago Kachinsky <tjk091@gmail.com>
2021-03-22 23:03:21 -0300

    Validação formulário

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const emailValue = document.getElementById('user-email-phone');
 buttonLogin.addEventListener('click', () => {
   alert(emailValue.value);
 });
+
+function verifyInputs() {
+  const registerButton = document.getElementById('facebook-register');
+  const inputs = document.querySelectorAll('.right-content input');
+  const errorMesage = document.getElementById('invalid-field');
+
+  registerButton.addEventListener('click', (e) => {
+    for (let i = 0; i < inputs.length; i += 1) {
+      if (inputs[i].value === '') {
+        errorMesage.innerHTML = 'Campos inválidos';
+        e.preventDefault();
+        // console.log(inputs);
+      }
+    }
+  });
+}
+
+verifyInputs();
