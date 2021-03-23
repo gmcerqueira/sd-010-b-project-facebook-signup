61b1c01bef75b809e348339d08178bffcfe2b3d9
Guilherme Lima <guilhermelimasky@gmail.com>
2021-03-23 00:55:33 -0300

    Alterando lint

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const email = document.getElementById('user-email-phone')
-const buttonHeader = document.getElementById('button-login')
+const email = document.getElementById('user-email-phone');
+const buttonHeader = document.getElementById('button-login');
 
-buttonHeader.addEventListener('click', buttonH)
+buttonHeader.addEventListener('click', buttonH);
 
-function buttonH (){
-    alert(email.value)
+function buttonH(){ 
+  alert(email.value);
 }
