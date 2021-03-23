f50000b91afe4e0acdc7719438a47623fe9adf71
Camila Arruda <camilaam1802@gmail.com>
2021-03-23 11:09:25 -0300

    testes

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ function botaoEntrar() {
 }
 
 buttonEntrar.addEventListener('click', botaoEntrar);
+
+const buttonCadastrar = document.getElementById('facebook-register');
+const userName = document.getElementById('name');
+const lastName = document.getElementById('lastname');
+const phoneEmail = document.getElementById('phone_email'); 
+const password = document.getElementById('password'); 
+const birthdate = document.getElementById('birthdate'); 
+const radioSelecionado = document.querySelector('input[name="gender"]:checked');
+
+function validarCadastro(){
+  if((userName.value === "") || (lastName.value === "") || (phoneEmail.value === "") || (password.value === "") || (birthdate.value === "") || (radioSelecionado.value === "" )){
+    alert('Campo inválido');
+  } else {
+    alert('Tudo ok'); 
+  }
+}
+
+buttonCadastrar.addEventListener('click', validarCadastro); 
+
