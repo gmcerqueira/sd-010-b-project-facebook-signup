ff5453f78a455c3ef1f24478680bb21d70e98fa9
FernandaPorto <fernanda.oliveira.mec@gmail.com>
2021-03-23 12:58:20 -0300

    Adiciona alterações de estilização do top bar, bem como tenta resolver problema do requisito 20 - de conteúdo múltiplo no right content

--- script.js
+++ script.js
@@ -34,22 +34,18 @@ masc.addEventListener('click', selectedGender);
 fem.addEventListener('click', selectedGender);
 personalizado.addEventListener('click', selectedGender);
 
-// Para transformar objeto em array, utilizamos como referência este site: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/values
-
 function printtext() {
   const rightContent = document.querySelector('.right-content');
   rightContent.innerHTML = '';
-  const createUl = document.createElement('ul');
-  rightContent.appendChild(createUl);
-  const recoverdInfo = JSON.parse(localStorage.formPreenchido);
-  createUl.innerText = `Olá, ${recoverdInfo.first} ${recoverdInfo.last}`;
-  const arrayOfInfos = Object.values(recoverdInfo);
-  for (let index = 3; index < arrayOfInfos.length; index += 1) {
-    const selectUl = document.querySelector('ul');
-    const createLi = document.createElement('li');
-    createLi.innerText = arrayOfInfos[index];
-    selectUl.appendChild(createLi);
-  }
+  const createH2 = document.createElement('h3');
+  rightContent.appendChild(createH2);
+  const recInfo = JSON.parse(localStorage.formPreenchido);
+  const birth = `Data de Nascimento: ${recInfo.birthDate}`;
+  const mail = `E-mail ou Telefone: ${recInfo.mail}`;
+  const gender = `Gênero: ${recInfo.gender}`;
+  const cool = 'Recebemos o teu cadastro com sucesso! Confira os seus dados:';
+  const infoData = `\n ${cool}\n ${mail}\n ${birth}\n ${gender}`;
+  createH2.innerText = `Olá, ${recInfo.first} ${recInfo.last}! ${infoData}`;
 }
 
 // Referêcia utilizada para realizar o requisito 20: https://stackoverflow.com/questions/2010892/storing-objects-in-html5-localstorage //
