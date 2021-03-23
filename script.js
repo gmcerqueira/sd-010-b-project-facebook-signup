a9cf0d6825e3c25b0d21e43eb0b9a3c111b8d0a4
Cristian <cristian.bugs@gmail.com>
2021-03-23 11:02:36 -0300

    eslint

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function textAlert() {
-    var textMsg = document.getElementById("user-email-phone").value;
-    alert(textMsg);
-  }
+  var textMsg = document.getElementById("user-email-phone").value;
+  alert(textMsg);
+}
