9259a7d05f223c304f742a9150c4505d5211d12d
Mariana Mohr <marianamohr@gmail.com>
2021-03-23 19:49:06 -0300

    Ajustado requisito campos invalidos

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ function alerta() {
   alert(email);
 }
 
+function validaDados() {
+  const name = document.getElementById('firstname');
+  const nameLastname = document.getElementById('lastname');
+  const phone = document.getElementById('phone_email');
+  const password = document.getElementById('password');
+  const birthdate = document.getElementById('birthdate');
+  if ((name.value === '') || (nameLastname.value === '') || (phone.value === '') || (password.value === '') || (birthdate.value === '')) {
+    const pai = document.getElementById('campo-obrigatorio');
+    const p = document.createElement('p');
+    p.innerHTML = 'Campos inválidos';
+    pai.appendChild(p);
+  }
+}
+
 document.getElementById('button-login').addEventListener('click', alerta);
-
- function validaDados( event) {
-const name  = document.getElementById('firstname');
-
- }
- document.getElementById('facebook-register').addEventListener('click',validaDados)
+document.getElementById('facebook-register').addEventListener('click', validaDados);
