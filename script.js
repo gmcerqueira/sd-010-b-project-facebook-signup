0f8e4ef44950d869a40200fa8b0edc40c34480c4
edersonrodara <edersonrodara@gmail.com>
2021-03-23 08:59:56 -0300

    testando evaluator

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function test() {
-  if (document.getElementById('user-email-phone').value === '') {
-    alert('digite o login');
-  }
-}
+// function test() {
+//   if (document.getElementById('user-email-phone').value === '') {
+//     alert('digite o login');
+//   }
+// }
 
-document.getElementById('button-login').addEventListener('click', test);
+// document.getElementById('button-login').addEventListener('click', test);
