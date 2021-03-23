a458b3551e77d0fc1d6ce8bfcf9a54d7b38d4ea9
bidaffonso <bidaffondo@gmail.com>
2021-03-23 17:47:04 -0400

    Commit com algumas legendas pra organizar o script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
+//Declarações:
 const submitBtn = document.getElementById('button-login');
 const user = document.getElementById('user-email-phone');
 
+//Funções:
 function alerta() {
   alert(user.value);
 }
-
-submitBtn.addEventListener('click', alerta);
 
 // function verificarTexto() {
 //   if (cadastro.firstname.value === '' || cadastro.lastname.value === '' || cadastro.phone_email.value === '' || cadastro.password.value === '' || cadastro.birthdate.value === '') {
 //      alert ("Campos inválidos");
 //   };
 // }
+
 // function verificarTexto(){
 //   if (cadastro.lastname.value == "")
 //   {alert ("Campos inválidos") }
 //   }
 // Peguei informações desse site: http://www.linhadecodigo.com.br/artigo/3503/configurando-funcoes-de-um-formulario-html-com-javascript.aspx#ixzz6ptmsOtje
+
+//Chamadas das funções:
+submitBtn.addEventListener('click', alerta);
