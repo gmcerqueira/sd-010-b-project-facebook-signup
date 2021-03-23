9c618518f06e1d3c43f970ba540500bedd92a8bc
André Arnoni <andre_arnoni@yahoo.com.br>
2021-03-22 21:42:44 -0300

    arrumado requisiçao 19

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-window.onload = function start(){
+function addGender () {
+  
+  let labelGender = document.getElementById('label-gender');
+  if (document.querySelector("#custom-gender").checked === true) {
+    let newInput = document.createElement('input');
+    labelGender.appendChild(newInput);
+    newInput.id = 'newInput';
+    newInput.name = 'gender-custom';
+    newInput.placeholder = 'Gênero (opcional)';
+    }
+}
 
-  let dateInput = document.getElementById('birthdate').DatePickerX
+function checkNewInput() {
+  let labelGender = document.getElementById('label-gender');
+  if(document.getElementById('newInput')) {
+    return true
+  } else {
+    addGender()
+  }
+}
+
+function alert1 () {
+  let login = document.getElementById('user-email-phone').value;
+  alert(login);
+}
+
+
+window.onload = function start(){
+  
   let signIn = document.querySelector('#button-login');
+  let btn = document.getElementById('custom-gender')
+  let dateInput = document.getElementById('birthdate').DatePickerX
 
   dateInput.init({
     mondayFirst: false,
@@ -13,10 +41,11 @@ window.onload = function start(){
   })
     
   
-  function alert1 () {
-    let login = document.getElementById('user-email-phone').value;
-    alert(login);
-  }
   
   signIn.addEventListener('click', alert1);
+  
+  
+  btn.addEventListener('click', checkNewInput)
+  
+
 }
