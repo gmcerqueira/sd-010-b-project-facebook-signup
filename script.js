6598f2c1346e7b3a4b69abd09acd2c28cecc2656
eraldoalmeida <eraldo.salmeida@gmail.com>
2021-03-23 11:25:50 -0300

    requisito 18/3

--- script.js
+++ script.js
@@ -2,22 +2,22 @@ document.getElementById('button-login').addEventListener('click', () => {
   alert(document.getElementById('user-email-phone').value);
 });
 
-const getTextInput = document.getElementsByClassName("inputForm");
-const formButton = document.getElementById('facebook-register')
+const getTextInput = document.getElementsByClassName('inputForm');
+const formButton = document.getElementById('facebook-register');
 
 function validateText() {
   let emptyInput = false;
-  for (let x=0; x < getTextInput.length; x += 1){
-    if (getTextInput[x].value == ''){
-      emptyInput = true
+  for (let x = 0; x < getTextInput.length; x += 1) {
+    if (getTextInput[x].value === '') {
+      emptyInput = true;
     }
   }
   return emptyInput;
 }
 
-formButton.addEventListener('click', (e) =>{ 
+formButton.addEventListener('click', (e) => {
   e.preventDefault();
-  if (validateText()){
-    document.getElementById('erro').innerText = 'Campos inválidos'
+  if (validateText()) {
+    document.getElementById('erro').innerText = 'Campos inválidos';
   }
-})
+});
