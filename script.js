a926eb7d3097847be8a07d63d64b42340ed1505d
Joao Vitor Fidanza <vitor.fidanza@gmail.com>
2021-03-22 22:46:34 -0300

    correçoes JS

--- script.js
+++ script.js
@@ -7,12 +7,28 @@ document.getElementById('button-login').addEventListener('click', alertEntrar);
 const radioButtonPersonalizado = document.getElementById('Personalizado');
 
 function createCustomGenderInput() {
-  const newInput = document.createElement('input');
-  newInput.type = 'text';
-  newInput.name = 'gender-custom';
-  newInput.placeholder = 'Gênero (opcional)';
-  const divCustomGender = document.querySelector('.custom-gender-container');
-  divCustomGender.appendChild(newInput);
+  const put = document.querySelector('.custom-gender-container').firstChild;
+  if (put == null) {
+    const newInput = document.createElement('input');
+    newInput.type = 'text';
+    newInput.name = 'gender-custom';
+    newInput.placeholder = 'Gênero (opcional)';
+    const divCustomGender = document.querySelector('.custom-gender-container');
+    divCustomGender.appendChild(newInput);
+  }
 }
 
 radioButtonPersonalizado.addEventListener('click', createCustomGenderInput);
+
+function removeCustomGenderInput() {
+  const div = document.querySelector('.custom-gender-container');
+  const input = div.firstChild;
+  if (input != null) {
+    div.removeChild(input);
+  }
+}
+
+const masc = document.getElementById('Masculino');
+masc.addEventListener('click', removeCustomGenderInput);
+const fem = document.getElementById('Feminino');
+fem.addEventListener('click', removeCustomGenderInput);
