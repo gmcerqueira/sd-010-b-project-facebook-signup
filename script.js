ca79f58a0ace4b4ce86db352f5814430562dc993
Dieghonm <dieghonm@gmail.com>
2021-03-23 02:33:21 -0300

    arruma quesito 18, tentatica com required

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const inputEmailPhone = document.getElementById('user-email-phone');
 const buttonGetIn = document.getElementById('button-login');
+const buttonRegister = document.getElementById('facebook-register');
 
 function buttonIn() {
   alert(inputEmailPhone.value);
 }
 
+function register() {
+  
+}
+
+
+buttonRegister.addEventListener('click', register)
 buttonGetIn.addEventListener('click', buttonIn);
