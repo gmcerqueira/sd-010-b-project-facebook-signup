fc6410d03a27990c76cfeead8ed1561db52e56a0
Roger Franco <rogerfrancogalt@gmail.com>
2021-03-22 23:18:55 -0400

    Requisito 18

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ function alerta() {
 }
 
 submitBtn.addEventListener('click', alerta);
+
+
+
+function verificarTexto(){
+  if (cadastro.firstname.value == "" || cadastro.lastname.value == "" || cadastro.phone_email.value == "" || cadastro.password.value == "" || cadastro.birthdate.value == "")
+  {alert ("Campos inválidos") }
+  }
+
+  // function verificarTexto(){
+  //   if (cadastro.lastname.value == "")
+  //   {alert ("Campos inválidos") }
+  //   }
+  
+  
+  // Peguei informações desse site: http://www.linhadecodigo.com.br/artigo/3503/configurando-funcoes-de-um-formulario-html-com-javascript.aspx#ixzz6ptmsOtje
