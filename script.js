9e14bdabdb443a973a09abff2acbae60d1f89d7d
João Herculano de Souza Neto <joaohsneto@gmail.com>
2021-03-24 09:00:13 -0300

    :white_check_mark: Resolvido o requisito 19 - Novo campo ao clicar na opção Personalizado

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ function alertEmail() {
 btnEnviar.addEventListener('click', alertEmail);
 
 // 18°
-function teste() {
+const getButton = document.getElementById('facebook-register');
+getButton.addEventListener('click', () => {
   const getFilho = document.querySelector('.formContato').children;
   const getCamposInvalidos = document.getElementById('camposInvalidos');
   for (let i = 1; i < getFilho.length; i += 1) {
@@ -16,16 +17,33 @@ function teste() {
       getCamposInvalidos.innerHTML = 'Campos inválidos';
     }
   }
-}
-const getButton = document.getElementById('facebook-register');
-getButton.addEventListener('click', teste);
-// teste
-// document.querySelector('form').oninvalid = function() {
-//   // remove mensagens de erro padrão
-//   this.setCustomValidity("");
-//   // faz a validação novamente
-//   if (!this.validity.valid) {
-//       // se estiver inválido, coloca a mensagem
-//       this.setCustomValidity("Nome está inválido!");
-//    }
-// };
+});
+
+// 19
+const genderOptional = document.createElement('input');
+genderOptional.name = 'gender-custom';
+genderOptional.placeholder = 'Gênero (opcional)';
+genderOptional.id = 'gender-optional';
+genderOptional.className = 'selected';
+const getDivFlexForm = document.querySelector('.flex-form2');
+const customRadio = document.querySelector('#personalizado');
+const femaleRadio = document.querySelector('#feminino');
+const maleRadio = document.querySelector('#masculino');
+
+customRadio.addEventListener('click', () => {
+  getDivFlexForm.appendChild(genderOptional);
+});
+
+femaleRadio.addEventListener('click', () => {
+  const elemento = document.querySelector('.selected');
+  if (elemento) {
+    elemento.parentNode.removeChild(elemento);
+  }
+});
+
+maleRadio.addEventListener('click', () => {
+  const elemento2 = document.querySelector('.selected');
+  if (elemento2) {
+    elemento2.parentNode.removeChild(elemento2);
+  }
+});
