7b68b72a8cfa4342a5bba7e2fe039896eb64142a
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-03-24 10:53:40 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -5,3 +5,33 @@ function alertaEmailOuTelefone() {
   alert(emailOuTelefone);
 }
 botaoEntrar.addEventListener('click', alertaEmailOuTelefone);
+// Reserva para refatorar o código do requisito 18=  let inputsCadastro = document.querySelectorAll(".cadastro input");
+
+const botaoCadastrar = document.getElementById('facebook-register');
+
+function validaInput() {
+  const inputs = document.querySelectorAll('.cad');
+  for (let i = 0; i < inputs.length; i += 1) {
+    if (inputs[i].value === '') {
+      alert('Campos inválidos');
+      inputs[i].focus();
+      return inputs[i];
+    }
+  }
+}
+
+function validaRadioButton() {
+  const gender = document.getElementsByClassName('radio-button');
+
+  if (gender[0].checked === false) {
+    if (gender[1].checked === false && gender[2].checked === false) {
+        alert('Campos inválidos');
+      }
+    }
+
+function valida() {
+  validaInput();
+  validaRadioButton();
+}
+
+botaoCadastrar.addEventListener('click', valida);
