f21c0f937587d7182672300e30c59504de5b4ebe
Breno Rodrigues <brenorm@gmail.com>
2021-03-24 03:23:10 -0300

    Requisito 10: Code review

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const enterButton = document.querySelector('#button-login');
 
-enterButton.addEventListener('click', () => { alert(document.getElementById('user-email-phone').value)});
+enterButton.addEventListener('click', () => {
+  alert(document.getElementById('user-email-phone').value);
+})
