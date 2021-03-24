68f775a5585a543008bcf50785cbfcead0f7a4e7
Anderson <trkz.anderson@gmail.com>
2021-03-24 10:11:37 -0300

    Ajustes na funções

--- script.js
+++ script.js
@@ -22,3 +22,26 @@ const showImput = document.getElementById('user-email-phone');
 buttonEnter.addEventListener('click', () => {
   alert(showImput.value);
 });
+
+function generoPersonalizado() {
+  const gender = document.getElementById('personalizado');
+  const section = document.getElementById('div-gender');
+  const input = document.createElement('input');
+
+  input.setAttribute('class', 'geral');
+
+  console.log(gender.checked)
+  if (gender.checked) {
+    section.appendChild(input);
+  }
+}
+
+function verificarGenero() {
+  const genders = document.getElementsByClassName('radio-gender');
+
+  for (let index = 0; index < 3; index++){
+    genders[index].addEventListener('click', generoPersonalizado);
+  }
+}
+
+verificarGenero();
