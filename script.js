5b02bc08c1bd41ea0dab29963dcf03c43023fd37
Roger Franco <rogerfrancogalt@gmail.com>
2021-03-24 16:18:13 -0400

    correçao Lint

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //  Declarações:
 const submitBtn = document.getElementById('button-login');
 const user = document.getElementById('user-email-phone');
+const registerBtn = document.getElementById('facebook-register');
+
 
 //  Funções:
 function alerta() {
@@ -8,13 +10,14 @@ function alerta() {
 }
 
 function verificarTexto() {
-  let campos = document.querySelectorAll('.teste')
+  const campos = document.querySelectorAll('.teste');
   campos.forEach((item, index) => {
-    if (item.value == '') {
-      campos[index].value = "Campos inválidos";
+    if (item.value === '') {
+      campos[index].value = 'Campos inválidos';
     }
-  })
+  });
 }
 
 //  Chamadas das funções:
 submitBtn.addEventListener('click', alerta);
+registerBtn.addEventListener('click', verificarTexto);
