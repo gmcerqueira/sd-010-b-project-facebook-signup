6a8807b2416782a46727459464a625249c648974
renatogsouza <renatomacedo1995@gmail.com>
2021-03-23 22:20:25 -0300

    adiciona input quando seleciona o genero personalizado

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const btnEntrar = document.getElementById('button-login');
 const emailTelefone = document.getElementById('user-name-phone');
+const personalizado = document.getElementById('Personalizado');
+const labelGender = document.getElementById('generos');
+const genderCustom = document.createElement('input');
 
-function alerta() {
-  alert(emailTelefone.value);
-}
+const createGenderCustom = () => labelGender.appendChild(genderCustom);
+const alerta = () =>  alert(emailTelefone.value);
 
 btnEntrar.addEventListener('click', alerta);
+personalizado.addEventListener('click', createGenderCustom);
