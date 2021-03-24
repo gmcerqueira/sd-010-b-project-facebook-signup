6aa2c87626e6e23125f9b3ec15004097402f1b8d
Matheus Bodra <matheusbodra@gmail.com>
2021-03-24 15:05:00 -0300

    refazendo o requisito 18

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-const botaoEntrar = document.getElementById('button-login');
+// Criamos a função que alerta o valor do campo Email ou Telefone e a atrelamos ao evento do botão de entrar ser clicado.
+
 function alertaEmailOuTelefone() {
   const emailOuTelefone = document.getElementById('user-email-phone').value;
   alert(emailOuTelefone);
 }
+
+const botaoEntrar = document.getElementById('button-login');
 botaoEntrar.addEventListener('click', alertaEmailOuTelefone);
-// Reserva para refatorar o código do requisito 18=  let inputsCadastro = document.querySelectorAll(".cadastro input");
-const botaoCadastrar = document.getElementById('facebook-register');
+
+// Atribuimos uma váriavel ao elemento <p></p> que receberá o texto "campo inválido" caso algum campo do formulário não esteja preenchido.
+
+const textoCamposInvalidos = document.getElementById('campos-invalidos');
+
 function validaInput() {
   const inputs = document.querySelectorAll('.cad');
   for (let i = 0; i < inputs.length; i += 1) {
     if (inputs[i].value === '') {
-      alert('Campos inválidos');
-      inputs[i].focus();
-      return inputs[i];
+      return true;
     }
   }
 }
+
 function validaRadioButton() {
   const gender = document.getElementsByClassName('radio-button');
   let opcoesSemCheck = 0;
@@ -25,12 +30,18 @@ function validaRadioButton() {
     }
   }
   if (opcoesSemCheck === 3) {
-    alert('Campos inválidos');
+    return true;
   }
 }
 
-function valida() {
-  validaInput();
-  validaRadioButton();
+// Se algum campo não estiver preenchido paramos a ação padrão do botão de cadastrar e mostrarmos a mensagem "Campos invalidos".
+
+function valida(event) {
+  if (validaInput() === true || validaRadioButton() === true) {
+    event.preventDefault();
+    textoCamposInvalidos.innerHTML = 'Campos invalidos';
+  }
 }
+
+const botaoCadastrar = document.getElementById('facebook-register');
 botaoCadastrar.addEventListener('click', valida);
