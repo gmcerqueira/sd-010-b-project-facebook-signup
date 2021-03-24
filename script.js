b502a0193844b07ac25a22c71aba9ff5fad8abfb
Letycia Barreto <letyciasbarreto@gmail.com>
2021-03-24 09:57:35 -0300

    R18

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-const button = document.getElementById('button-login');
 function alertButton() {
   const emailTel = document.getElementById('user-email-phone');
   alert(emailTel.value);
 }
-button.addEventListener('click', alertButton);
+document.getElementById('button-login').addEventListener('click', alertButton);
+
+function checks() {
+  const verifica = document.querySelectorAll(':required');
+  for (let i = 0; i < verifica.length; i += 1) {
+    if (verifica[i].value === '') {
+      const formInput = document.getElementById('campos');
+      formInput.innerHTML = ' Campos inválidos';
+      return false;
+    }
+  }
+}
+document.getElementById('facebook-register').addEventListener('click', checks);
