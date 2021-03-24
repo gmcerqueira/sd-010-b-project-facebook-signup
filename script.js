d52b38e6121ee50aa5af996a5d65d81a19babc54
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-03-24 14:39:44 -0300

    espaços

--- script.js
+++ script.js
@@ -7,50 +7,50 @@ const mainContent = document.querySelector('.main-content');
 const rightForm = document.querySelector('.right-content');
 
 function mostrarElemento() {
-    personalizado.style.display = 'flex';
+  personalizado.style.display = 'flex';
 }
 
 function alertEmail() {
-    alert(`${document.getElementById('user-email-phone').value}`);
+  alert(`${document.getElementById('user-email-phone').value}`);
 }
 
 function userLoged(props) {
-    const gender = document.querySelector('input[type="radio"]:checked').value;
-    mainContent.removeChild(rightForm);
-    const div = document.createElement('div');
-    mainContent.appendChild(div);
-    const elemento = document.createElement('aside');
-    elemento.className = 'right-content';
-    mainContent.appendChild(elemento);
-    elemento.innerHTML = `<h1>Olá, ${props[0]} ${props[1]}</h1>`;
-    elemento.innerHTML += `<p>${props[2]}</p>`;
-    elemento.innerHTML += `<p>${props[3]}</p>`;
-    elemento.innerHTML += `<p>${gender}</p>`;
+  const gender = document.querySelector('input[type="radio"]:checked').value;
+  mainContent.removeChild(rightForm);
+  const div = document.createElement('div');
+  mainContent.appendChild(div);
+  const elemento = document.createElement('aside');
+  elemento.className = 'right-content';
+  mainContent.appendChild(elemento);
+  elemento.innerHTML = `<h1>Olá, ${props[0]} ${props[1]}</h1>`;
+  elemento.innerHTML += `<p>${props[2]}</p>`;
+  elemento.innerHTML += `<p>${props[3]}</p>`;
+  elemento.innerHTML += `<p>${gender}</p>`;
 }
 
 function checarCampo() {
-    const nome = document.getElementById('nomeLogin').value;
-    const sobrenome = document.getElementById('sobrenomeLogin').value;
-    const telefone = document.getElementById('telefoneLogin').value;
-    const nascimento = document.getElementById('nascimentoLogin').value;
-    const senha = document.getElementById('senhaLogin').value;
-    const elementos = [nome, sobrenome, telefone, nascimento, senha];
-    for (let index = 0; index < elementos.length; index += 1) {
-        if (elementos[index] === '') {
-            campo.style.display = 'flex';
-            campo.innerText = 'Campos inválidos';
-            break;
-        }
+  const nome = document.getElementById('nomeLogin').value;
+  const sobrenome = document.getElementById('sobrenomeLogin').value;
+  const telefone = document.getElementById('telefoneLogin').value;
+  const nascimento = document.getElementById('nascimentoLogin').value;
+  const senha = document.getElementById('senhaLogin').value;
+  const elementos = [nome, sobrenome, telefone, nascimento, senha];
+  for (let index = 0; index < elementos.length; index += 1) {
+    if (elementos[index] === '') {
+      campo.style.display = 'flex';
+      campo.innerText = 'Campos inválidos';
+      break;
     }
-    if (campo.innerText !== 'Campos inválidos') {
-        userLoged(elementos);
-    }
+  }
+  if (campo.innerText !== 'Campos inválidos') {
+    userLoged(elementos);
+  }
 }
 
 function initalizeApp() {
-    botaoLogin.addEventListener('click', alertEmail);
-    botao.addEventListener('click', checarCampo);
-    personalizadoRadio.addEventListener('click', mostrarElemento);
+  botaoLogin.addEventListener('click', alertEmail);
+  botao.addEventListener('click', checarCampo);
+  personalizadoRadio.addEventListener('click', mostrarElemento);
 }
 
 initalizeApp();
