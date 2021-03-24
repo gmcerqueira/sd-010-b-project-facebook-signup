b3983b2aeb9928e884d2999b5fdac20e4aaf6505
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-24 12:17:43 -0300

    esLint

--- script.js
+++ script.js
@@ -4,45 +4,45 @@ enterButton.addEventListener('click', () => {
   alert(document.getElementById('user-email-phone').value);
 });
 
- const form = document.getElementById('formulario');
+const form = document.getElementById('formulario');
 
- form.addEventListener('submit', validador);
+form.addEventListener('submit', validador);
 
- function validador (){
- var Cadname = document.getElementById("name");
- var Cadlastname = document.getElementById("sName");
- var Cadphone_email = document.getElementById("foEmail");
- var Cadpassword = document.getElementById("foSenha");
- var Cadbirthdate = document.getElementById("label-birthdate");
- var Cadradio = document.querySelector('input[name="gender"]:checked');
+function validador (){
+var Cadname = document.getElementById("name");
+var Cadlastname = document.getElementById("sName");
+var Cadphone_email = document.getElementById("foEmail");
+var Cadpassword = document.getElementById("foSenha");
+var Cadbirthdate = document.getElementById("label-birthdate");
+var Cadradio = document.querySelector('input[name="gender"]:checked');
 
-    if (Cadname.value == "") {
-      alert("Campos inválidos");
-      return;
-    }
+  if (Cadname.value == "") {
+    alert("Campos inválidos");
+    return;
+  }
 
-    if (Cadlastname.value == "") {
-      alert("Campos inválidos");
-      return;
-    }
+  if (Cadlastname.value == "") {
+    alert("Campos inválidos");
+    return;
+  }
 
-    if (Cadphone_email.value == "") {
-      alert("Campos inválidos");
-      return;
-    }
+  if (Cadphone_email.value == "") {
+    alert("Campos inválidos");
+    return;
+  }
 
-    if (Cadpassword.value == "") {
-      alert("Campos inválidos");
-      return;
-    }
+  if (Cadpassword.value == "") {
+    alert("Campos inválidos");
+    return;
+  }
 
-    if (Cadbirthdate.value == "") {
-      alert("Campos inválidos");
-      return;
-    }
+  if (Cadbirthdate.value == "") {
+    alert("Campos inválidos");
+    return;
+  }
     
-    if (Cadradio == null) {
-      alert("Campos inválidos");
-      return;
-    }
+  if (Cadradio == null) {
+    alert("Campos inválidos");
+    return;
+  }
 }
