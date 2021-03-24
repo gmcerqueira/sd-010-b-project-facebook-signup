79d399c518dd2d04be68f916310b31736a29515c
Alexandre Damasceno <ale.damasceno.silva@gmail.com>
2021-03-24 14:14:53 -0300

    Refatora requisito 16

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 const botaoEntrar = document.getElementById('button-login');
-
 function alertaEmailOuTelefone() {
   const emailOuTelefone = document.getElementById('user-email-phone').value;
   alert(emailOuTelefone);
 }
 botaoEntrar.addEventListener('click', alertaEmailOuTelefone);
 // Reserva para refatorar o código do requisito 18=  let inputsCadastro = document.querySelectorAll(".cadastro input");
-
 const botaoCadastrar = document.getElementById('facebook-register');
-
 function validaInput() {
   const inputs = document.querySelectorAll('.cad');
   for (let i = 0; i < inputs.length; i += 1) {
@@ -19,25 +16,20 @@ function validaInput() {
     }
   }
 }
-
 function validaRadioButton() {
-    const gender = document.getElementsByClassName('radio-button');
-    let opcoesSemCheck = 0;
-    
-    for (let i = 0; i < gender.length; i += 1) {
+  const gender = document.getElementsByClassName('radio-button');
+  let opcoesSemCheck = 0;
+  for (let i = 0; i < gender.length; i += 1) {
     if (gender[i].checked === false) {
-    opcoesSemCheck += 1;
+      opcoesSemCheck += 1;
     }
-    }
-    if (opcoesSemCheck === 3) {
+  }
+  if (opcoesSemCheck === 3) {
     alert('Campos inválidos');
-    }
-} 
-
-
+  }
+}
 function valida() {
   validaInput();
   validaRadioButton();
 }
-
 botaoCadastrar.addEventListener('click', valida);
