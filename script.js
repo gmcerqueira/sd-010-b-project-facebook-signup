1010326bede3626b726db1eecb35670ccd9080c0
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-03-24 13:56:42 -0300

    18. Validar se todos os campos foram preenchidos ao clicar no botão Cadastre-se

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const buttonLogin = document.querySelector('#button-login');
 const catchValueInput = document.querySelector('#user-email-phone');
+const registerInputs = document.querySelectorAll('.input-register');
+const registerButton = document.querySelector('#facebook-register');
 
 buttonLogin.addEventListener('click', () => {
   if (catchValueInput.value !== '') {
@@ -7,4 +9,32 @@ buttonLogin.addEventListener('click', () => {
   } else {
     alert('Campo Vazio');
   }
+});
+
+// validates all input fields
+function validatesInputs() {
+  let isValid = true;
+
+  for (let i = 0; i < registerInputs.length; i += 1) {
+    if (!registerInputs[i].value) {
+      isValid = false;
+    }
+  }
+
+  return isValid;
+}
+
+// validates the whole form
+function validatesForm() {
+  const errorField = document.querySelector('#error-message');
+
+  if (!validatesInputs()) {
+    errorField.innerHTML = 'Campos inválidos';
+  }
+}
+
+registerButton.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  validatesForm();
 });
