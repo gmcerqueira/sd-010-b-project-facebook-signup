3bce7b659704da1719fb7b83cb7c022ba7c0de6d
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-03-23 21:42:44 -0300

    white_check_mark: Desafio 18 - Adicionando form de campos invalidos

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ function alertEmail() {
 }
 
 btnEnviar.addEventListener('click', alertEmail);
+
+// 18°
+function teste() {
+  const getFilho = document.querySelector('.formContato').children;
+  for (let i = 1; i < getFilho.length; i += 1) {
+    if (getFilho[i].value === '') {
+      alert('Campos inválidos');
+    }
+  }
+}
+const getButton = document.getElementById('facebook-register');
+getButton.addEventListener('click', teste);
+// teste
+// document.querySelector('form').oninvalid = function() {
+//   // remove mensagens de erro padrão
+//   this.setCustomValidity("");
+//   // faz a validação novamente
+//   if (!this.validity.valid) {
+//       // se estiver inválido, coloca a mensagem
+//       this.setCustomValidity("Nome está inválido!");
+//    }
+// };
