d293add996a993a1a100005cf8d259b78060d54b
Jander Liborio <jandernunes14@gmail.com>
2021-03-24 14:36:05 -0400

    script.js tirado o espaço

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const buttomLogin = document.getElementById('button-login');
 const loginValue = document.getElementById('user-email-phone');
 buttomLogin.addEventListener('click', () => {
-    alert(loginValue.value);
+  alert(loginValue.value);
 });
