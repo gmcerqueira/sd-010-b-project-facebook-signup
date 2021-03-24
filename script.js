98a6a39efbf72286176aa1697953bb7731953773
Arlen Freitas <arlenfreitas1@gmail.com>
2021-03-24 15:27:49 -0300

    fix line

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ const labelGender = document.getElementById('generos');
 const genderCustom = document.createElement('input');
 
 const createGenderCustom = () => labelGender.appendChild(genderCustom);
-const alerta = () => alert(`${document.getElementById('user-email-phone').value}`);
+
+function alerta() {
+  alert(`${document.getElementById('user-email-phone').value}`);
+} 
 
 btnEntrar.addEventListener('click', alerta);
 personalizado.addEventListener('click', createGenderCustom);
